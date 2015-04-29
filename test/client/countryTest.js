82a18246012a4c704f66175f64e34e6c34828aff
Al3xCore <alex-kern@online.de>
2015-04-29 22:38:33 +0200

    added testcase

--- test/client/countryTest.js
+++ test/client/countryTest.js
@@ -7,12 +7,19 @@
 TestCase("CountryTest", {
     
     setUp: function () {
-      this.country1 = Object.create(tddjs.client.map.Country);  
+      this.country1 = Object.create(tddjs.client.map.Country); 
+      this.country2 = Object.create(tddjs.client.map.Country); 
     },
 
   "test object of country should not be undefined": function () { 
       
       assertObject(this.country1);
-  }  
+  },
+  "test if country1 should store country2 as a border": function () { 
+      
+     assertFalse(this.country1.getBorderCount() == 0);
+     this.country1.addBorder(this.country2);
+     assertTrue(this.country1.getBorderCount() > 0);
+  }
   
 });
