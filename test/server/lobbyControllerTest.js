789e8169473dded4f057604bf16c91ecec3dd52e
y7haar <y7haar@gmail.com>
2015-04-30 22:55:38 +0200

    Added LobbyController Testcases

--- test/server/lobbyControllerTest.js
+++ test/server/lobbyControllerTest.js
@@ -5,12 +5,19 @@
 TestCase("LobbyControllerTest", {
     
     setUp: function () {
-      this.lobbyController = Object.create(tddjs.server.controller.LobyController);
+      this.lobbyController = Object.create(tddjs.server.controller.LobbyController);
+      this.lobby1 = {};
     },
 
   "test object of LobbyController should not be undefined": function () { 
       
       assertObject(this.lobbyController);
+  },
+  
+    "test lobbyController should store lobby1": function () { 
+      assertEquals(0, this.lobbyController.getLobbyCount());
+      this.lobbyController.addLobby(this.lobby1);
+       assertEquals(1, this.lobbyController.getLobbyCount());
   }
   
 });
