3b8661f52a86544345ea87a22aeb6fa99f210b7d
y7haar <y7haar@gmail.com>
2015-04-30 23:12:17 +0200

    Refactored Testcases for LobbyController

--- test/server/lobbyControllerTest.js
+++ test/server/lobbyControllerTest.js
@@ -7,6 +7,7 @@ TestCase("LobbyControllerTest", {
     setUp: function () {
       this.lobbyController = Object.create(tddjs.server.controller.LobbyController);
       this.lobby1 = {};
+      this.lobby2 = {};
     },
 
   "test object of LobbyController should not be undefined": function () { 
@@ -14,18 +15,27 @@ TestCase("LobbyControllerTest", {
       assertObject(this.lobbyController);
   },
   
-    "test lobbyController should store lobby1": function () { 
+    "test lobbyController should store lobby1 and lobby2": function () { 
       assertEquals(0, this.lobbyController.getLobbyCount());
       this.lobbyController.addLobby(this.lobby1);
        assertEquals(1, this.lobbyController.getLobbyCount());
+       this.lobbyController.addLobby(this.lobby2);
+       assertEquals(2, this.lobbyController.getLobbyCount());
   },
   
-      "test lobbyController should hold a reference to lobby1": function () { 
-      assertNotSame(this.lobby1, this.lobbyController.getLobby());
+      "test lobbyController should hold a reference to lobby1 and lobby2": function () { 
+      assertNotSame(this.lobby1, this.lobbyController.getLobbies()[0]);
+      assertNotSame(this.lobby1, this.lobbyController.getLobbies()[1]);
+      assertNotSame(this.lobby2, this.lobbyController.getLobbies()[0]);
+      assertNotSame(this.lobby2, this.lobbyController.getLobbies()[1]);
       
       this.lobbyController.addLobby(this.lobby1);
       
-       assertSame(this.lobby1, this.lobbyController.getLobby());
+       assertSame(this.lobby1, this.lobbyController.getLobbies()[0]);
+       
+       this.lobbyController.addLobby(this.lobby2);
+       
+       assertSame(this.lobby2, this.lobbyController.getLobbies()[1]);
   }
   
 });
