8ecb2621853981ba273536c42a6b13f21a4e63a5
y7haar <y7haar@gmail.com>
2015-05-01 16:02:00 +0200

    Updated LobbyTest

--- test/server/lobbyTest.js
+++ test/server/lobbyTest.js
@@ -8,15 +8,17 @@ TestCase("LobbyTest", {
       this.lobby = Object.create(tddjs.server.model.Lobby);
       this.player1 = {};
       this.player2 = {};
+      this.player3 = {};
     },
 
   "test object of Lobby should not be undefined": function () { 
        assertObject(this.lobby);
   },
   
-    "test Lobby should store player1 and player2": function () { 
+    "test Lobby should store player1 and player2 and player 3": function () { 
         assertFalse(this.lobby.getPlayers().indexOf(this.player1) >= 0);
         assertFalse(this.lobby.getPlayers().indexOf(this.player2) >= 0);
+        assertFalse(this.lobby.getPlayers().indexOf(this.player3) >= 0);
         
         this.lobby.addPlayer(this.player1);
         assertTrue(this.lobby.getPlayers().indexOf(this.player1) >= 0);
@@ -24,6 +26,31 @@ TestCase("LobbyTest", {
         this.lobby.addPlayer(this.player2);
         assertTrue(this.lobby.getPlayers().indexOf(this.player1) >= 0);
         assertTrue(this.lobby.getPlayers().indexOf(this.player2) >= 0);
+        
+        this.lobby.addPlayer(this.player3);
+        assertTrue(this.lobby.getPlayers().indexOf(this.player1) >= 0);
+        assertTrue(this.lobby.getPlayers().indexOf(this.player2) >= 0);
+        assertTrue(this.lobby.getPlayers().indexOf(this.player3) >= 0);
+  },
+  
+      "test Lobby should store player1 and player2, but not player3 because the player limit is reached": function () { 
+        this.lobby.setMaxPlayers(2);
+          
+        assertFalse(this.lobby.getPlayers().indexOf(this.player1) >= 0);
+        assertFalse(this.lobby.getPlayers().indexOf(this.player2) >= 0);
+        assertFalse(this.lobby.getPlayers().indexOf(this.player3) >= 0);
+        
+        this.lobby.addPlayer(this.player1);
+        assertTrue(this.lobby.getPlayers().indexOf(this.player1) >= 0);
+        
+        this.lobby.addPlayer(this.player2);
+        assertTrue(this.lobby.getPlayers().indexOf(this.player1) >= 0);
+        assertTrue(this.lobby.getPlayers().indexOf(this.player2) >= 0);
+        
+        this.lobby.addPlayer(this.player3);
+        assertTrue(this.lobby.getPlayers().indexOf(this.player1) >= 0);
+        assertTrue(this.lobby.getPlayers().indexOf(this.player2) >= 0);
+        assertFalse(this.lobby.getPlayers().indexOf(this.player3) >= 0);
   }
   
 });
