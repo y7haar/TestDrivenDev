d39c6437c94c510696236cf504d2c1ac766dec53
y7haar <y7haar@gmail.com>
2015-05-01 15:56:18 +0200

    Updated Lobby

--- src/server/lobby.js
+++ src/server/lobby.js
@@ -4,9 +4,21 @@
 
 (function ()
 {
+        var _players = [];
+    
+        function addPlayer(aPlayer)
+        {
+            _players.push(aPlayer);
+        }
+        
+        function getPlayers()
+        {
+            return _players;
+        }
     
         tddjs.namespace("server.model").Lobby = {
-     
+            addPlayer: addPlayer,
+            getPlayers: getPlayers
         };
     
 }());
