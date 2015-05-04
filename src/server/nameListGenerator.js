27af9a211c97968ddd00aa351327685c8ad242d9
y7haar <y7haar@gmail.com>
2015-05-04 23:46:00 +0200

    Updated NameListGenerator

--- src/server/nameListGenerator.js
+++ src/server/nameListGenerator.js
@@ -3,8 +3,21 @@
  */
 (function ()
 {
+        var _nameList = [];
+        
+        function setNameList(aNameList)
+        {
+            _nameList = aNameList;
+        }
+        
+        function getNameList()
+        {
+            return _nameList;
+        }
     
         tddjs.namespace("server.controller").NameListGenerator = {
+            setNameList: setNameList,
+            getNameList: getNameList
         };
     
 }());
