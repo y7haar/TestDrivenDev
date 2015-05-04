aa87ad65cb109e838bf67169e0cbc5b5a515b2ee
René Lottes <rene.lottes@hof-university.de>
2015-05-04 21:23:47 +0200

    Continent

---
+++ src/client/continent.js
@@ -0,0 +1,30 @@
+/* 
+Source-Code for Continent
+ */
+
+
+(function()
+{
+    var _land ;
+    
+    function addCountry(aCountry)
+    {
+      
+    }
+    
+    function getCountryCount()
+    {
+
+    }
+    
+    function getCountrys()
+    {
+  
+    }
+    
+    tddjs.namespace("client.map").Continent = {
+        addBorder: addCountry,
+        getBorderCount: getCountryCount,
+        borders: getCountrys
+    };    
+}());
