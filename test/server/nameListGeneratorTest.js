b7eaf95365e1a42d9ee4ef3a8157342d2410606d
y7haar <y7haar@gmail.com>
2015-05-04 23:57:02 +0200

    Updated NameListGeneratorTest

--- test/server/nameListGeneratorTest.js
+++ test/server/nameListGeneratorTest.js
@@ -8,6 +8,10 @@ TestCase("NameListGeneratorTest", {
         this.nameListGenerator = Object.create(tddjs.server.controller.NameListGenerator);
         this.initialArray = [1, 2, 3, 4];
     },
+    
+    tearDown: function () {
+        this.nameListGenerator.getNameList().length = 0;
+    },
 
   "test object of NameListGenerator should not be undefined": function () { 
        assertObject(this.nameListGenerator);
@@ -15,11 +19,23 @@ TestCase("NameListGeneratorTest", {
   
     "test NameListGenerator should store a given array": function () { 
         assertNotSame(this.initialArray, this.nameListGenerator.getNameList());
-        
         this.nameListGenerator.setNameList(this.initialArray);
-        
         assertSame(this.initialArray, this.nameListGenerator.getNameList());
- 
+  },
+  
+      "test given array should be randomly shuffled and still contain every value": function () { 
+        this.nameListGenerator.setNameList(this.initialArray);
+        this.nameListGenerator.shuffleNameList();
+         
+        this.shuffled = this.nameListGenerator.getNameList();
+        
+        /*
+         *  tests if the array still holds every given element
+         */
+        assertTrue(this.shuffled[0] === 1 || this.shuffled[1] === 1 || this.shuffled[2] === 1 || this.shuffled[3] === 1);
+        assertTrue(this.shuffled[0] === 2 || this.shuffled[1] === 2 || this.shuffled[2] === 2 || this.shuffled[3] === 2);
+        assertTrue(this.shuffled[0] === 3 || this.shuffled[1] === 3 || this.shuffled[2] === 3 || this.shuffled[3] === 3);
+        assertTrue(this.shuffled[0] === 4 || this.shuffled[1] === 4 || this.shuffled[2] === 4 || this.shuffled[3] === 4);
   }
   
 });
