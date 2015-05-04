bf395b7992cbadcd6798c8d83a86ac056f11e54a
y7haar <y7haar@gmail.com>
2015-05-05 00:09:32 +0200

    Updated NameListGenerator

--- src/server/nameListGenerator.js
+++ src/server/nameListGenerator.js
@@ -14,10 +14,27 @@
         {
             return _nameList;
         }
+        
+        function shuffleNameList()
+        {
+            var currentIndex = _nameList.length;
+            
+            while(currentIndex > 0)
+            {
+                // calculate new random index
+                var randomIndex = Math.floor(Math.random() * currentIndex--);
+                
+                //  swap elements
+                var swapValue = _nameList[currentIndex];
+                _nameList[currentIndex] = _nameList[randomIndex];
+                _nameList[randomIndex] = swapValue;
+            }
+        }
     
         tddjs.namespace("server.controller").NameListGenerator = {
             setNameList: setNameList,
-            getNameList: getNameList
+            getNameList: getNameList,
+            shuffleNameList: shuffleNameList
         };
     
 }());
