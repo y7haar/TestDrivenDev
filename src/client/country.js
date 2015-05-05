7965c2f746747f7ad1118669fa02cafd347edd43
Al3xCore <alex-kern@online.de>
2015-05-05 19:53:57 +0200

    refactored country.js

--- src/client/country.js
+++ src/client/country.js
@@ -11,55 +11,57 @@ tddjs.namespace("client.map");
         this._unitCount = 1;
     }
     tddjs.client.map.Country = Country;
-}());
-
-(function()
-{
-    var _border = [] ;
-    var _unitCount = 1;
     
     function addBorder(aCountry)
     {
         _border.push(aCountry);
-      
     }
-    
+
     function getBorderCount()
     {
         return _border.length;
     }
-    
+
     function borders(aCountry)
     {
-        if( _border.indexOf(aCountry) >= 0) return true;        
-        else return false;
+        if (_border.indexOf(aCountry) >= 0)
+            return true;
+        else
+            return false;
     }
-    
+
     function getUnitCount()
     {
         return _unitCount;
     }
-    
+
     function addUnits(count)
     {
-        if(isNaN(count))throw new TypeError("Parameter is not a number");
-        else _unitCount += count;
+        if (isNaN(count))
+            throw new TypeError("Parameter is not a number");
+        else
+            _unitCount += count;
     }
-    
+
     function subUnits(count)
     {
-        if(isNaN(count))throw new TypeError("Parameter is not a number");
-        
-        if( _unitCount - count < 1 ) _unitCount == 1;
-        else _unitCount -= count;
-    } 
+        if (isNaN(count))
+            throw new TypeError("Parameter is not a number");
+
+        if (_unitCount - count < 1)
+            _unitCount == 1;
+        else
+            _unitCount -= count;
+    }
+
+    Country.prototype.addBorder = addBorder;
+    Country.prototype.getBorderCount = getBorderCount;
+    Country.prototype.borders = borders;
+    Country.prototype.getUnitcount = getUnitCount;
+    Country.prototype.addUnits = addUnits;
+    Country.prototype.subUnits = subUnits;    
     
-    tddjs.namespace("client.map").Country = {
-        addBorder: addBorder,
-        getBorderCount: getBorderCount,
-        borders: borders,
-        getUnitCount: getUnitCount,
-        addUnits: addUnits,
-        subUnits: subUnits
-    };    
 }());
+
+    
+  
