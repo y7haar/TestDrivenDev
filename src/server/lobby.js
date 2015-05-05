1e9e3ca39d5074258765ebd18c088b41390160f5
y7haar <y7haar@gmail.com>
2015-05-05 20:49:53 +0200

    Refactored Lobby

--- src/server/lobby.js
+++ src/server/lobby.js
@@ -2,50 +2,59 @@
  *  Source-Code for Lobby
  */
 
-(function ()
-{
-        var _players = [];
-        var _maxPlayers;
-    
-        function addPlayer(aPlayer)
-        {
-            if(_players.length < _maxPlayers)
-                 _players.push(aPlayer);
-        }
-        
-        function getPlayers()
-        {
-            return _players;
-        }
-        
-        /*
-         *  Sets max player count and kicks players, if current player count > maximum
-         */
-        function setMaxPlayers(aMaxPlayers)
-        {
-            _maxPlayers = aMaxPlayers;
+tddjs.namespace("server.model");
 
-            while(_players.length > aMaxPlayers)
-            {
-                    _players.pop();
-            }
-        }
-        
-        function kickPlayer(aPlayer)
+(function()
+{
+    var _players = [];
+    var _maxPlayers;
+
+
+    // Constructor
+    function Lobby(aId)
+    {
+
+    }
+
+    tddjs.server.model.Lobby = Lobby;
+
+    function addPlayer(aPlayer)
+    {
+        if (_players.length < _maxPlayers)
+            _players.push(aPlayer);
+    }
+
+    function getPlayers()
+    {
+        return _players;
+    }
+
+    /*
+     *  Sets max player count and kicks players, if current player count > maximum
+     */
+    function setMaxPlayers(aMaxPlayers)
+    {
+        _maxPlayers = aMaxPlayers;
+
+        while (_players.length > aMaxPlayers)
         {
-            
-            var index = _players.indexOf(aPlayer);
-            
-            if(index >= 0)
-                _players.splice(index, 1);
+            _players.pop();
         }
-    
-        tddjs.namespace("server.model").Lobby = {
-            addPlayer: addPlayer,
-            kickPlayer: kickPlayer,
-            getPlayers: getPlayers,
-            setMaxPlayers: setMaxPlayers
-        };
-    
+    }
+
+    function kickPlayer(aPlayer)
+    {
+
+        var index = _players.indexOf(aPlayer);
+
+        if (index >= 0)
+            _players.splice(index, 1);
+    }
+
+    Lobby.prototype.addPlayer = addPlayer;
+    Lobby.prototype.getPlayers = getPlayers;
+    Lobby.prototype.setMaxPlayers = setMaxPlayers;
+    Lobby.prototype.kickPlayer = kickPlayer;
+
 }());
 
