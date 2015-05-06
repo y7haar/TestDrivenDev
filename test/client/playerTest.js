645bcaa05cd12defee26957e4ef3beabaacf568d
Ralf Heinrich <ralf.heinrich@hof-university.de>
2015-05-06 14:36:49 +0200

    Setter verbessert

--- test/client/playerTest.js
+++ test/client/playerTest.js
@@ -14,9 +14,12 @@ TestCase("PlayerTest", {
         assertNotUndefined(this.player1.getName());
     },
     
-    "test Playername should be able to setted and getted": function() {
+    "test Playername should be able to setted and getted correctly": function() {
         this.player1.setName("Ranol");
         assertEquals("Ranol", this.player1.getName());
+        assertException(function(){this.player1.setName(Undefined);},"TypeError");
+        assertException(function(){this.player1.setName(125);},"TypeError");
+        assertException(function(){this.player1.setName(this.player1);},"TypeError");
     }
 });
 
