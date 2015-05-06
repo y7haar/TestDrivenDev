92f45b715ea2a9f1ea07d201584a45058fc01dd6
y7haar <y7haar@gmail.com>
2015-05-06 22:24:39 +0200

    Updated LobbyControllerTest

--- test/server/lobbyControllerTest.js
+++ test/server/lobbyControllerTest.js
@@ -6,8 +6,11 @@ TestCase("LobbyControllerTest", {
     
     setUp: function () {
       this.lobbyController = Object.create(tddjs.server.controller.LobbyController);
-      this.lobby1 = {};
-      this.lobby2 = {};
+      this.lobby1 = Object.create(tddjs.server.model.Lobby);
+      this.lobby2 = Object.create(tddjs.server.model.Lobby);
+      
+      this.lobby1.setId(1);
+      this.lobby2.setId(2);
     },
     
     tearDown: function(){
@@ -15,7 +18,6 @@ TestCase("LobbyControllerTest", {
     },
 
   "test object of LobbyController should not be undefined": function () { 
-      
         assertObject(this.lobbyController);
   },
   
@@ -25,6 +27,23 @@ TestCase("LobbyControllerTest", {
          assertEquals(1, this.lobbyController.getLobbyCount());
          this.lobbyController.addLobby(this.lobby2);
          assertEquals(2, this.lobbyController.getLobbyCount());
+  },
+  
+   "test lobbyController should return a specific Lobby by Id ": function () { 
+        this.lobbyController.addLobby(this.lobby1);
+        this.lobbyController.addLobby(this.lobby2);
+        
+        assertSame(this.lobby1, this.lobbyController.getLobbyById(1));
+        assertSame(this.lobby2, this.lobbyController.getLobbyById(2));
+  },
+  
+   "test lobbyController should return null if lobbyController does not store an element with the given id ": function () {     
+        assertNull(this.lobbyController.getLobbyById(5));
+  },
+  
+  "test lobbyController should throw Exception if parameter is no number ": function () { 
+        var lobbyController = this.lobbyController;
+        assertException(function() {lobbyController.getLobbyById("asd");}, "TypeError");
   },
   
       "test lobbyController should hold a reference to lobby1 and lobby2": function () { 
