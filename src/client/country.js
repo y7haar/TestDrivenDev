3022587a5935b552e8581a1cf0a210f53f5ba950
Al3xCore <alex-kern@online.de>
2015-05-06 21:40:33 +0200

    Refactored

--- src/client/country.js
+++ src/client/country.js
@@ -61,7 +61,7 @@ tddjs.namespace("client.map");
     }
     
     function setOwner(aOwner)
-    {
+    {     
         if(typeof aOwner != "object")
             throw new TypeError("setOwner expects a Player object as Parameter");
         _owner = aOwner;
