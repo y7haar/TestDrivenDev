982d920ed82008d14ef3f67dd38787c8525b61ca
Al3xCore <alex-kern@online.de>
2015-05-06 21:34:07 +0200

    Refactored

--- src/client/country.js
+++ src/client/country.js
@@ -7,23 +7,23 @@ tddjs.namespace("client.map");
 (function()
 {
    
-    this._border = [];
-    this._unitCount = 1;
-    this._owner; 
+    var _border = [];
+    var _unitCount = 1;
+    var _owner; 
     
     function addBorder(aCountry)
     {
-        this._border.push(aCountry);
+        _border.push(aCountry);
     }
 
     function getBorderCount()
     {
-        return this._border.length;
+        return _border.length;
     }
 
     function borders(aCountry)
     {
-        if (this._border.indexOf(aCountry) >= 0)
+        if (_border.indexOf(aCountry) >= 0)
             return true;
         else
             return false;
@@ -31,7 +31,7 @@ tddjs.namespace("client.map");
 
     function getUnitCount()
     {
-        return this._unitCount;
+        return _unitCount;
     }
 
     function addUnits(count)
@@ -39,7 +39,7 @@ tddjs.namespace("client.map");
         if (isNaN(count))
             throw new TypeError("Parameter is not a number");
         else
-            this._unitCount += count;
+            _unitCount += count;
     }
 
     function subUnits(count)
@@ -48,28 +48,28 @@ tddjs.namespace("client.map");
             throw new TypeError("Parameter is not a number");
 
         if (this._unitCount - count < 1)
-            this._unitCount = 1;
+            _unitCount = 1;
         else
-            this._unitCount -= count;
+            _unitCount -= count;
     }
     
     function setUnitCount(count)
     {
         if(count < 1 )
             throw new Error("Count forbbided to be under 1");
-       this._unitCount = count ;   
+        _unitCount = count ;   
     }
     
     function setOwner(aOwner)
     {
         if(typeof aOwner != "object")
             throw new TypeError("setOwner expects a Player object as Parameter");
-        this._owner = aOwner;
+        _owner = aOwner;
     }
     
     function getOwner()
     {
-        return this._owner;
+        return _owner;
     }
     
     tddjs.namespace("client.map").Country = {    
