aade0c5ab133b99700430a294da0993d65b352c9
Ralf Heinrich <ralf.heinrich@hof-university.de>
2015-05-06 21:15:27 +0200

    Tet  für Hinzufügen von Ländern hinzugefügt

--- test/client/playerTest.js
+++ test/client/playerTest.js
@@ -1,9 +1,11 @@
  /* 
  * Testcases for Player
  */
-TestCase("PlayerTest", {
+TestCase("PlayerTest",
+{
     setUp: function () {
-        this.player1 = Object.create(tddjs.client.Player); 
+        this.player1 = Object.create(tddjs.client.Player);
+        this.country1 = new tddjs.client.map.Country();
     },
     
     "test Player Object should be created": function () {
@@ -14,16 +16,25 @@ TestCase("PlayerTest", {
         assertNotUndefined(this.player1.getName());
     },
     
-    "test Playername should be able to setted and getted correctly": function() {
+    "test Playername should be able to setted and getted correctly": function() 
+    {
+        var test;
+        var player = this.player1;
         this.player1.setName("Ranol");
         assertEquals("Ranol", this.player1.getName());
-        assertException(function(){this.player1.setName(Undefined);},"TypeError");
-        assertException(function(){this.player1.setName(125);},"TypeError");
-        assertException(function(){this.player1.setName(this.player1);},"TypeError");
+        assertException(function(){player.setName(test);},"TypeError");
+        assertException(function(){player.setName(125);},"TypeError");
+        assertException(function(){player.setName(this.player1);},"TypeError");
     },
     
-    "test Shouldnt be Able to ask for Countries which arent Countries": function() {
-        assertException(function(){this.player1.hasCountry("Fischi");}, "TypeError");
-    }
+    "test Shouldnt be able to ask for Countries which arent countries": function() {
+        var player = this.player1;
+        assertException(function(){ player.hasCountry({});},"Error");
+    },
+    
+    "test Should be able to add a Country": function() {
+        var player = this.player1;
+        player.addCountry();
+    }  
 });
 
