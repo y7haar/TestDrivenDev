e2986cc5dab28fb5ed7df84e06358f7f2623c5b3
Ralf Heinrich <ralf.heinrich@hof-university.de>
2015-05-07 12:33:42 +0200

    Testfälle zum entfernen eines Landes und hasCountry() hinzugefügt

--- test/client/playerTest.js
+++ test/client/playerTest.js
@@ -6,6 +6,7 @@ TestCase("PlayerTest",
     setUp: function () {
         this.player1 = Object.create(tddjs.client.Player);
         this.country1 = Object.create(tddjs.client.map.Country);
+        this.country2 = Object.create(tddjs.client.map.Country);
     },
     
     "test Player Object should be created": function () {
@@ -27,16 +28,17 @@ TestCase("PlayerTest",
         assertException(function(){player.setName(this.player1);},"TypeError");
     },
     
-    "test Shouldnt be able to ask for Countries which arent countries": function() {
+    "test Shouldnt be able to ask for Countries which arent countries": function()
+    {
         var player = this.player1;
         assertException(function(){ player.hasCountry({});}, "TypeError");
     },
     
     "test Should be able to add a Country and find it afterwards": function()
     {
+        assertFalse(this.player1.hasCountry(this.country1));
         this.player1.addCountry(this.country1);
-        var x = this.player1.hasCountry(this.country1);
-        assertTrue(x);
+        assertTrue(this.player1.hasCountry(this.country1));
     },
     
     "test Shouldnt be able to add something thats not a Country": function()
@@ -44,6 +46,22 @@ TestCase("PlayerTest",
         var player = this.player1;
         var test = 5;
         assertException(function(){player.addCountry(test);});
+    },
+    
+    "test Shouldnt be able to remove something thats not a Country": function()
+    {
+        var test = 5;
+        assertException(this.player1.removeCountry(test));
+        assertException(this.player1.removeCountry(this.player1));
+    },
+    
+    "test Shouldnt be able to add and remove a Country. A removed Country should not be found": function()
+    {
+        assertFalse(this.player1.hasCountry(this.country2));
+        this.player1.addCountry(this.country2);
+        assertTrue(this.player1.hasCountry(this.country2));
+        this.player1.removeCountry(this.country1);
+        assertFalse(this.player1.hasCountry(this.country2));
     }
 });
 
