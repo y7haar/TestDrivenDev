e9d6051c4085085109f86ce7cb789bbdeb61df2c
Ralf Heinrich <ralf.heinrich@hof-university.de>
2015-05-07 12:21:26 +0200

    Testfall zum Erstellen des Objektes Map hinzugefügt

--- test/client/mapTest.js
+++ test/client/mapTest.js
@@ -5,7 +5,12 @@ TestCase("Map-Test",
 {
     setUp: function ()
     {
-     
+        this.map1 = Object.create(tddjs.client.Map);
     },
+    
+    "test Map Object should be created": function()
+    {
+        assertObject(this.map1);
+    }
 });
 
