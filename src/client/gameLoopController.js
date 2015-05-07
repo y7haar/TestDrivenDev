fee08071ba56121bd2dfc9366526000a71a57f41
Al3xCore <alex-kern@online.de>
2015-05-08 01:00:38 +0200

    updated gameLoopController added code for getter and setter of player and map

--- src/client/gameLoopController.js
+++ src/client/gameLoopController.js
@@ -7,4 +7,36 @@ tddjs.namespace("client.game").gameLoopController = gameLoopController;
 
 function gameLoopController()
 {
+    var _players = [];
+    var _map;
+    
+    function setMap(aMap)
+    {
+        if(aMap instanceof tddjs.client.Map)
+            throw new TypeError("Parameter ist not instance of Map");
+        this._map = aMap;
+    }
+    
+    function getMap()
+    {
+        return _map;
+    }
+    
+    function addPlayer(aPlayer)
+    {
+        if(aPlayer instanceof tddjs.client.Player)
+            throw new TyperError("Parameter ist not instance of Player");
+        this._player.push(aPlayer);
+    }
+    
+    function getPlayers()
+    {
+        return this._players;
+    }
+    
+    
+    this.setMap = setMap;
+    this.getMap = getMap;
+    this.addPlayer = addPlayer;
+    this.getPlayers = getPlayers;
 }
