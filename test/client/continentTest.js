27d5076b1bd194eca8e21fb992ac55d60116a72e
Al3xCore <alex-kern@online.de>
2015-05-08 14:42:42 +0200

    refactored continentTest.js

--- test/client/continentTest.js
+++ test/client/continentTest.js
@@ -7,8 +7,11 @@ TestCase("ContinentTest", {
 
     setUp: function () {
         this.country1 = new tddjs.client.map.country();
+        this.country1.setName("Bangladesh");
         this.country2 = new tddjs.client.map.country();
+        this.country2.setName("Uganda");
         this.country3 = new tddjs.client.map.country();
+        this.country3.setName("Guantemala");
         this.continent1 = new tddjs.client.map.continent();
     },
     tearDown: function () {
@@ -23,6 +26,11 @@ TestCase("ContinentTest", {
     },
     "test if Continent1 stores country objects,exception if not country instance": function () {
 
+        assertFunction(this.continent1.getCountrys);
+        assertFunction(this.continent1.getCountryCount);
+        assserFunction(this.continent1.addCountry);
+        assertfunciton(this.continent1.getCountry);
+        
         assertEquals([],this.continent1.getCountrys());
         assertEquals(0,this.continent1.getCountryCount());
         
@@ -49,15 +57,27 @@ TestCase("ContinentTest", {
         assertEquals([this.country1,this.country2, this.country3], this.continent1.getCountrys());
         assertEquals(3,this.continent1.getCountryCount());
         
+        assertSame(this.country1,this.continent1.getCountry(this.country.getName()));
+        assertNotSame(this.country2, this.continent1.getCountry("TakatukaLand"));
+        
     },    
     "test if contrys are in Continent1": function () {
+        
+        assertFunction(this.continent1.hasCountryByObject);
+        assertFunciton(this.continent1.hasCountryByName);
+        
         
         this.continent1.addCountry(this.country1);
         this.continent1.addCountry(this.country3);
         
-        assertTrue(this.continent1.hasCountry(this.country1));
-        assertFalse(this.continent1.hasCountry(this.country2));
-        assertTrue(this.continent1.hasCountry(this.country3));
+        assertTrue(this.continent1.hasCountryByObject(this.country1));
+        assertFalse(this.continent1.hasCountryByObject(this.country2));
+        assertTrue(this.continent1.hasCountryByObject(this.country3));
+        
+        assertTrue(this.continent1.hasCountryByName(this.country1.getName()));
+        assertFalse(this.continent1.hasCountryByName(this.country2.getName()));
+        assertTrue(this.continent1.hasCountryByName(this.country3.getName()));
+        
     },
     
     "test setter/getter of unitBonus": function () {
