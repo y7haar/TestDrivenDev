67ddf283b0b61b04d3e22b16e525ccfc1d8aa81c
y7haar <y7haar@gmail.com>
2015-05-09 00:41:40 +0200

    Formatted NameListGenerator

--- src/server/nameListGenerator.js
+++ src/server/nameListGenerator.js
@@ -6,37 +6,37 @@ tddjs.namespace("server.controller").nameListGenerator =  nameListGenerator;
 
 function nameListGenerator()
 {
-        var _nameList = [];
-        
-        function setNameList(aNameList)
+    var _nameList = [];
+
+    function setNameList(aNameList)
+    {
+        _nameList = aNameList;
+    }
+
+    function getNameList()
+    {
+        return _nameList;
+    }
+
+    function shuffleNameList()
+    {
+        var currentIndex = _nameList.length;
+
+        while(currentIndex > 0)
         {
-            _nameList = aNameList;
+            // calculate new random index
+            var randomIndex = Math.floor(Math.random() * currentIndex--);
+
+            //  swap elements
+            var swapValue = _nameList[currentIndex];
+            _nameList[currentIndex] = _nameList[randomIndex];
+            _nameList[randomIndex] = swapValue;
         }
-        
-        function getNameList()
-        {
-            return _nameList;
-        }
-        
-        function shuffleNameList()
-        {
-            var currentIndex = _nameList.length;
-            
-            while(currentIndex > 0)
-            {
-                // calculate new random index
-                var randomIndex = Math.floor(Math.random() * currentIndex--);
-                
-                //  swap elements
-                var swapValue = _nameList[currentIndex];
-                _nameList[currentIndex] = _nameList[randomIndex];
-                _nameList[randomIndex] = swapValue;
-            }
-        }
-    
-        this.setNameList = setNameList;
-        this.getNameList = getNameList;
-        this.shuffleNameList = shuffleNameList;
+    }
+
+    this.setNameList = setNameList;
+    this.getNameList = getNameList;
+    this.shuffleNameList = shuffleNameList;
     
 };
 
