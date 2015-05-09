ed65e711369d527728541efb9395d01e7cf99dc7
y7haar <y7haar@gmail.com>
2015-05-09 21:38:04 +0200

    Added testcase for xhr object xhr object should have important xhr functions

--- test/client/ajaxTest.js
+++ test/client/ajaxTest.js
@@ -15,7 +15,15 @@ TestCase("AjaxStubTest", {
     "test xhr object should not be undefined": function () {  
         assertObject(this.xhr);
         assertTrue(this.xhr instanceof tddjs.stubs.ajax);
+    },
+    
+    "test fake xhr should have all important XMLHttpRequest functions": function () {  
+        assertFunction(this.xhr.open());
+        assertFunction(this.xhr.onreadystatechange());
+        assertFunction(this.xhr.send());
+        assertFunction(this.xhr.setRequestHeader());
     }
+    
 });
 
 
