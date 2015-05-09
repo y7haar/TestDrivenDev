9ecc5ac4a57b152ecd57d14d92276a44ef36cfcd
y7haar <y7haar@gmail.com>
2015-05-09 14:48:24 +0200

    Succesfully implemented getter for new lobby instrances

--- src/server/lobbyFactory.js
+++ src/server/lobbyFactory.js
@@ -6,6 +6,17 @@ tddjs.namespace("server.controller").lobbyFactory =  lobbyFactory;
 
 function lobbyFactory()
 {
+    var _lobbyController = tddjs.server.controller.lobbyController.getInstance();
     
+    function getNewLobby()
+    {
+        var lobby = new tddjs.server.model.lobby();
+        var id = _lobbyController.getNextId();
+        lobby.setId(id);
+        
+        return lobby;
+    }
+    
+    this.getNewLobby = getNewLobby;
 };
 
