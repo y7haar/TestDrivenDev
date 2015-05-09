739107aee21bcaa26e86bf168910f3ee9f272a11
y7haar <y7haar@gmail.com>
2015-05-09 22:34:02 +0200

    Added testcase for GET request in ajax Added testcase for POST request in ajax

--- test/client/ajaxTest.js
+++ test/client/ajaxTest.js
@@ -5,7 +5,6 @@
 TestCase("AjaxStubTest", {
     setUp: function () {
         this.xhr = new tddjs.stubs.ajax();
-
     }, 
     tearDown: function ()
     {
@@ -17,15 +16,57 @@ TestCase("AjaxStubTest", {
         assertTrue(this.xhr instanceof tddjs.stubs.ajax);
     },
     
-    "test fake xhr should have all important XMLHttpRequest functions": function () {  
+    "test fake xhr should have all important XMLHttpRequest functions and member attributes": function () {  
         assertFunction(this.xhr.open);
         assertFunction(this.xhr.onreadystatechange);
         assertFunction(this.xhr.send);
         assertFunction(this.xhr.setRequestHeader);
+        
+        assertNotUndefined(this.xhr.responseText);
+    }
+});
+
+TestCase("AjaxStubGETTest", {
+    setUp: function () {
+        this.xhr = new tddjs.stubs.ajax();
+        this.url = "/url";
+        this.method = "GET";
+        this.async = true;
+
+    }, 
+    tearDown: function ()
+    {
+        delete this.xhr;
+    },
+    
+    "test if functions are called in correct order and parameters are set correctly": function () {  
+        var xhr = this.xhr;
+        
+        assertException(function() { xhr.open(); }, "Error");
+        
+        assertNoException(function() { xhr.open("GET", "/url", true); });
+        assertNoException(function() { xhr.send(); });
     }
     
 });
 
+TestCase("AjaxStubPOSTTest", {
+    setUp: function () {
+        this.xhr = new tddjs.stubs.ajax();
+        this.url = "/url";
+        this.method = "POST";
+        this.async = true;
+        
+        this.header = "content-type:";
+        this.value = "plain/text";
 
-
-
+    }, 
+    tearDown: function ()
+    {
+        delete this.xhr;
+    },
+    
+    "test if functions are called in correct order and parameters are set correctly": function () {  
+        
+    }
+});
