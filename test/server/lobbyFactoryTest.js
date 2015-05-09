bb1c5d73c5b9f2ca870032eb05f1fe5012da3de8
y7haar <y7haar@gmail.com>
2015-05-09 14:40:13 +0200

    Added test for getting new Instances of Lobby

--- test/server/lobbyFactoryTest.js
+++ test/server/lobbyFactoryTest.js
@@ -9,10 +9,22 @@ TestCase("LobbyFactoryTest", {
     },
     
     tearDown: function () {
+        delete this.lobbyFactory;
     },
 
   "test object of LobbyFactory should not be undefined": function () { 
        assertObject(this.lobbyFactory);
+  },
+  
+   "test factory should provide getter for new unique instances of Lobby with correct Id": function () { 
+        assertFunction(this.lobbyFactory.getNewLobby());
+
+        this.lobby1 = this.lobbyFactory.getNewLobby();
+        this.lobby2 = this.lobbyFactory.getNewLobby();
+        
+        assertNotSame(this.lobby1, this.lobby2);
+        assertEquals(1, this.lobby1.getId());
+        assertEquals(2, this.lobby2.getId());
   }
   
 });
