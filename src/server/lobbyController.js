1a9c79db3033195e4f3b8c0e5317441ec6ac9756
y7haar <y7haar@gmail.com>
2015-05-09 14:23:01 +0200

    Formatted and removed deprecated stuff

--- src/server/lobbyController.js
+++ src/server/lobbyController.js
@@ -5,7 +5,6 @@
 tddjs.namespace("server.controller").lobbyController =  lobbyController;
 tddjs.namespace("server.controller.lobbyController").getInstance =  getInstance;
 
-
 var _lobbyControllerInstance = null;
 
 function getInstance()
@@ -19,7 +18,6 @@ function lobbyController()
 {
     var _lobbies = [];
     var _count = 0;
-   // var _instance = null;
     
     function addLobby(aLobby)
     {
@@ -82,14 +80,6 @@ function lobbyController()
         return id;
     }
     
-//    function getInstance()
-//    {
-//        if(_instance === null)
-//            _instance = new tddjs.server.controller.lobbyController();
-//        
-//        return _instance;
-//    }
-    
         this.addLobby =  addLobby;
         this.removeLobby = removeLobby;
         
@@ -98,6 +88,5 @@ function lobbyController()
         
         this.getLobbyById = getLobbyById;
         this.getNextId = getNextId;
-        
-        //this.getInstance = getInstance;
+
 };
