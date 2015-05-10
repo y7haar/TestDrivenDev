1f15a1e67e367e0d173d8ba5258191655af63af9
Ralf Heinrich <ralf.heinrich@hof-university.de>
2015-05-10 13:48:31 +0200

    Test für korrektes Zählen des Country-Count hinzugefügt

--- test/client/playerTest.js
+++ test/client/playerTest.js
@@ -25,7 +25,7 @@ TestCase("PlayerTest",
         assertEquals("Ranol", this.player1.getName());
         assertException(function(){player.setName(test);},"TypeError");
         assertException(function(){player.setName(125);},"TypeError");
-        assertException(function(){player.setName(this.player1);},"TypeError");
+        assertException(function(){player.setName(player);},"TypeError");
     },
     
     "test Shouldnt be able to ask for Countries which arent countries": function()
@@ -34,18 +34,18 @@ TestCase("PlayerTest",
         assertException(function(){ player.hasCountry({});}, "TypeError");
     },
     
-    "test Should be able to add a Country and find it afterwards": function()
-    {
-        assertFalse(this.player1.hasCountry(this.country1));
-        this.player1.addCountry(this.country1);
-        assertTrue(this.player1.hasCountry(this.country1));
-    },
-    
     "test Shouldnt be able to add something thats not a Country": function()
     {
         var player = this.player1;
         var test = 5;
         assertException(function(){player.addCountry(test);},"TypeError");
+    },
+    
+    "test Should be able to add a Country and find it afterwards": function()
+    {
+        assertFalse(this.player1.hasCountry(this.country1));
+        this.player1.addCountry(this.country1);
+        assertTrue(this.player1.hasCountry(this.country1));
     },
     
     "test Shouldnt be able to remove something thats not a Country": function()
@@ -56,19 +56,31 @@ TestCase("PlayerTest",
         assertException(function(){player.removeCountry(player);},"TypeError");
     },
     
-    "test Shouldnt be able to add and remove a Country. A removed Country should not be found": function()
+    "test Should be able to add and remove a Country. A removed Country should not be found": function()
     {
         assertFalse(this.player1.hasCountry(this.country2));
         this.player1.addCountry(this.country2);
         assertTrue(this.player1.hasCountry(this.country2));
-        this.player1.removeCountry(this.country1);
+        this.player1.removeCountry(this.country2);
         assertFalse(this.player1.hasCountry(this.country2));
+    },
+    
+    "test Shouldnt be able to remove a Country that has net been added": function() {
+        var player = this.player1;
+        var country2 = this.country2
+        assertException(function(){player.removeCountry(country2)}, "Error");
     },
     
     "test Should be able to get a valid Country-Count": function()
     {
         var x = this.player1.getCountryCount();
-        assertTrue(x>0);
+        assertTrue(x>=0);
+    },
+    
+    "test Should be able to get a correct CountryCount": function()
+    {
+        var x = this.player1.getCountryCount();
+        assertTrue(x === 1);
     }
 });
 
