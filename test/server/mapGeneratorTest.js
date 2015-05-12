79596bf59cd7fc0328518dad10bcf51db062fd95
René Lottes <rene.lottes@hof-university.de>
2015-05-12 08:39:18 +0200

    added mapGeneratorTest.js

---
+++ test/server/mapGeneratorTest.js
@@ -0,0 +1,15 @@
+/* 
+ * Tests for MapGenerator
+ */
+
+
+TestCase("NameListGeneratorTest", {
+    
+    setUp: function () {
+        this.mapGenerator = new tddjs.server.controller.nameListGenerator();
+    },
+    
+    tearDown: function () {
+
+    }
+});
