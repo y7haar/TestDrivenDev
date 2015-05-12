c3a860876d7e4672d3dedab77ac88ffd5df4a148
René Lottes <rene.lottes@hof-university.de>
2015-05-12 09:27:16 +0200

    added Test for GridSize

--- test/server/mapGeneratorTest.js
+++ test/server/mapGeneratorTest.js
@@ -7,6 +7,8 @@ TestCase("MapGeneratorTest", {
     
     setUp: function () {
         this.mapGenerator = new tddjs.server.controller.mapGenerator();
+        this.x = 5;
+        this.y = 5;
     },
     
     tearDown: function () {
@@ -15,5 +17,11 @@ TestCase("MapGeneratorTest", {
     
     "test object of MapGenerator should not be undefined": function () { 
        assertObject(this.mapGenerator);
+    },
+    
+    "test if MapGenerator can set a GridSize": function() {
+        this.mapGenerator.setGridSize(this.x,this.y)
+        assertEquals(this.mapGenerator.getMapWidth(),this.x);
+        assertEquals(this.mapGenerator.getMapHeight(),this.y);
     }
 });
