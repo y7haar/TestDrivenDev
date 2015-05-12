7035c8e27be64eb20fa405f6e712e71914b1de5b
René Lottes <rene.lottes@hof-university.de>
2015-05-12 08:21:40 +0200

    added mainloop in canvas.js

--- src/client/canvas.js
+++ src/client/canvas.js
@@ -3,14 +3,36 @@
  */
 
 var canvas;
+var ctx;
+var val=0.0;
+
+
+
+var mainloop = function() {
+        updateGame();
+        drawGame();
+    };
+
+function updateGame(){
+    
+}
+function drawGame(){
+    clear();
+}
+
+function clear(){
+    ctx.fillStyle = "#ebebeb";
+    ctx.fillRect(0,0,ctx.canvas.width, ctx.canvas.height);
+}
+
 
 function init(){
     canvas = document.getElementById('game');
     if (canvas && canvas.getContext) {
-       var ctx = canvas.getContext("2d");
+       ctx = canvas.getContext("2d");
        if (ctx) {
-          ctx.fillStyle = "#ebebeb";
-          ctx.fillRect(0,0,ctx.canvas.width, ctx.canvas.height);
+            clear();
+            window.requestAnimationFrame(mainloop);
         }
     }
 }
