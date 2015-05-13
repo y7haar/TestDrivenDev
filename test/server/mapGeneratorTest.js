ca6789aeee5e228f488c20df56d75a5c3be2b439
Ralf Heinrich <ralf.heinrich@hof-university.de>
2015-05-13 22:25:45 +0200

    Test zum Grid-Objekt Hinzugefügt. Tests zur GenerateMap-Funktion hinzugefügt.

--- test/server/mapGeneratorTest.js
+++ test/server/mapGeneratorTest.js
@@ -9,6 +9,7 @@ TestCase("MapGeneratorTest", {
         this.mapGenerator = new tddjs.server.controller.mapGenerator();
         this.x = 5;
         this.y = 5;
+        this.map = this.mapGenerator.generateMap();
     },
     
     tearDown: function () {
@@ -20,8 +21,27 @@ TestCase("MapGeneratorTest", {
     },
     
     "test if MapGenerator can set a GridSize": function() {
-        this.mapGenerator.setGridSize(this.x,this.y)
+        assertFunction(this.mapGenerator.setGridSize);
+        assertFunction(this.mapGenerator.getMapHeight);
+        assertFunction(this.mapGenerator.getMapWidth);
+        this.mapGenerator.setGridSize(this.x,this.y);
         assertEquals(this.mapGenerator.getMapWidth(),this.x);
         assertEquals(this.mapGenerator.getMapHeight(),this.y);
+    },
+    
+    "test grid should be a object": function()
+    {
+        assertObject(this.mapGenerator.getMapGrid());
+    },
+    
+    "test generateMap() should return a Map-Object":  function(){
+        assertFunction(this.mapGenerator.generateMap);
+        assertObject(this.map);
+        assertTrue(this.map instanceof tddjs.client.map.map);
+    },
+    
+    "test generateMap should return a map-object with atleast one continent": function()
+    {
+        assertTrue(this.map.getContinentCount() > 0);
     }
 });
