a0ff443ddeefea41125c5dde615bab9f06f79fcb
Al3xCore <alex-kern@online.de>
2015-05-13 22:50:19 +0200

    refactored eventSourceStubTest.js onmessage, onerror and onopen are not functions now, as they are in real Eventsource (at the beginning)

--- test/client/eventSourceStubTest.js
+++ test/client/eventSourceStubTest.js
@@ -2,6 +2,12 @@
  * Source-code for eventSourceStubTest
  * this code tests the simulation of the EventSource Object 
  */
+// to be deleted only for testing
+        console.log("_______________________");
+        var test = new EventSource("someURL1");     
+        console.log(test);        
+        console.log("___________________");
+
 
 TestCase("eventSourceStubTest", {
     setUp: function () {
@@ -15,40 +21,19 @@ TestCase("eventSourceStubTest", {
     "test eventSource object should not be undefined": function () {  
         assertObject(this.eventSourceObject);
         assertTrue(this.eventSourceObject instanceof tddjs.stubs.eventSourceStub);
-        //console.log(this.eventSourceObject);
     },
     
     "test onmessage, onerror and onopen function should not be undefined": function () {  
         assertNotUndefined(this.eventSourceObject.onmessage);
         assertNotUndefined(this.eventSourceObject.onerror);
         assertNotUndefined(this.eventSourceObject.onopen);
-        //console.log(this.eventSourceObject);
     },
     
     "test constants CONNECTING 0 , OPEN 1 and CLOSED 2 should retrn right values": function () {  
         assertEquals(0,tddjs.stubs.eventSourceStub.CONNECTING);
         assertEquals(1,tddjs.stubs.eventSourceStub.OPEN);
         assertEquals(2,tddjs.stubs.eventSourceStub.CLOSED);
-    },
-    "test events shoulde be called == return true": function () {  
-        assertFalse(this.eventSourceObject.onmessage.called);
-        assertFalse(this.eventSourceObject.onopen.called);
-        assertFalse(this.eventSourceObject.onerror.called);
-        
-        var event = {msg:"some Message from Server"};
-        
-        this.eventSourceObject.onmessage(event);
-        this.eventSourceObject.onmessage(event);
-        this.eventSourceObject.onmessage(event);
-        
-        assertTrue(this.eventSourceObject.onmessage.called);
-        assertTrue(this.eventSourceObject.onopen.called);
-        assertTrue(this.eventSourceObject.onerror.called);
-        
-        
-    }
-   
-    
+    }   
 });
 
 
