5f47d473c70f192345474fc3a61f262de20851e8
Al3xCore <alex-kern@online.de>
2015-05-14 21:28:35 +0200

    eventSource now can hold  url

--- src/client/eventSourceStub.js
+++ src/client/eventSourceStub.js
@@ -9,18 +9,26 @@ tddjs.namespace("stubs").eventSourceStub.CLOSED = 2;
 tddjs.namespace("stubs").eventSourceStub.CONNECTING = 0;
 tddjs.namespace("stubs").eventSourceStub.OPEN = 1;
 
-function eventSource()
+function eventSource(url)
 {
+    var _url = url;
+    
     function addEventListner(eventName, eventFunction, useCapture)
     {
         var name = "on"+eventName.toLowerCase();
         this[name] = eventFunction;  
     }
     
+    function getURL()
+    {
+        return _url;
+    }
+    
     this.onopen = null;
     this.onmessage = null;
     this.onerror = null;
     this.addEventListner = addEventListner;
+    this.getURL = getURL;
 };
 
 
