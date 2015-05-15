5f14fb390dd2302884c2dfcbbd67e170080e8b8b
Yannic Siebenhaar <y7haar@gmail.com>
2015-05-15 14:31:42 +0200

    Added getter for xhr object in ajax facade

--- src/client/ajax.js
+++ src/client/ajax.js
@@ -7,6 +7,15 @@ tddjs.namespace("stubs").ajax = ajax;
 
 function ajax()
 {
+    var _xhr = new tddjs.stubs.ajax.xmlHttpRequest();
+    
+    function getXmlHttpRequest()
+    {
+        return _xhr;
+    }
+    
+    this.getXmlHttpRequest = getXmlHttpRequest;
+    
 };
 
 
