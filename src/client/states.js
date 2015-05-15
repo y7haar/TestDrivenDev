6f1b3dde51db66b9d579ab3a554ab2164ac760ba
Al3xCore <alex-kern@online.de>
2015-05-15 02:40:07 +0200

    created state.js this file collects alle states from the statePattern that is used for the gameLoopController it is now possible to crate instances of the specific State

---
+++ src/client/states.js
@@ -0,0 +1,53 @@
+/* 
+ * Source-code of the StatePattern for the gameLoopController
+ * 
+ *  4-States: PlaceingState, AttackingState, MovingState, WaitingState
+ *  all inherit from one State function
+ */
+
+
+tddjs.namespace("client").placeingState = placeingState;
+tddjs.namespace("client").attackingState = attackingState;
+tddjs.namespace("client").movingState = movingState;
+tddjs.namespace("client").waitingState = waitingState;
+
+function state()
+{
+    
+}
+state.prototype.placeUnits= null;
+state.prototype.endPlaceingPhase= null;
+state.prototype.attack = null;
+state.prototype.endAttackingPhase = null;
+state.prototype.moveUnits= null;
+state.prototype.endMoveingPhase= null;
+state.prototype.getUpdates= null;
+state.prototype.isMoveLegal= null;
+
+function placeingState()
+{
+    
+}
+placeingState.prototype = new state();
+placeingState.prototype.constructor = placeingState;
+
+function attackingState()
+{
+    
+}
+attackingState.prototype = new state();
+attackingState.prototype.constructor = attackingState;
+
+function movingState()
+{
+    
+}
+movingState.prototype = new state();
+movingState.prototype.constructor = movingState;
+
+function waitingState()
+{
+    
+}
+waitingState.prototype = new state();
+waitingState.prototype.constructor = waitingState;
