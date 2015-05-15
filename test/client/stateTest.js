e9cf06e4646746122917a624933b2cc10c4675e5
Al3xCore <alex-kern@online.de>
2015-05-15 02:56:55 +0200

    added testcase that test if alle states inherit all propertys

--- test/client/stateTest.js
+++ test/client/stateTest.js
@@ -37,7 +37,51 @@ TestCase("stateTest", {
         assertTrue(this.moving instanceof tddjs.client.abstractState);
         assertTrue(this.attacking instanceof tddjs.client.abstractState);
         assertTrue(this.waiting instanceof tddjs.client.abstractState);       
+    },
+    
+    "test states should have all functions": function () {
+        
+        assertNotUndefined(this.placeing.placeUnits);
+        assertNotUndefined(this.placeing.endPlaceingPhase);
+        assertNotUndefined(this.placeing.attack);
+        assertNotUndefined(this.placeing.endAttackingPhase);
+        assertNotUndefined(this.placeing.moveUnits);
+        assertNotUndefined(this.placeing.endMovingPhase);
+        assertNotUndefined(this.placeing.endWaitingPhase);
+        assertNotUndefined(this.placeing.getUpdates);
+        assertNotUndefined(this.placeing.isMoveLegal);
+
+        assertNotUndefined(this.attacking.placeUnits);
+        assertNotUndefined(this.attacking.endPlaceingPhase);
+        assertNotUndefined(this.attacking.attack);
+        assertNotUndefined(this.attacking.endAttackingPhase);
+        assertNotUndefined(this.attacking.moveUnits);
+        assertNotUndefined(this.attacking.endMovingPhase);
+        assertNotUndefined(this.attacking.endWaitingPhase);
+        assertNotUndefined(this.attacking.getUpdates);
+        assertNotUndefined(this.attacking.isMoveLegal);
+
+        assertNotUndefined(this.moving.placeUnits);
+        assertNotUndefined(this.moving.endPlaceingPhase);
+        assertNotUndefined(this.moving.attack);
+        assertNotUndefined(this.moving.endAttackingPhase);
+        assertNotUndefined(this.moving.moveUnits);
+        assertNotUndefined(this.moving.endMovingPhase);
+        assertNotUndefined(this.moving.endWaitingPhase);
+        assertNotUndefined(this.moving.getUpdates);
+        assertNotUndefined(this.moving.isMoveLegal);
+
+        assertNotUndefined(this.waiting.placeUnits);
+        assertNotUndefined(this.waiting.endPlaceingPhase);
+        assertNotUndefined(this.waiting.attack);
+        assertNotUndefined(this.waiting.endAttackingPhase);
+        assertNotUndefined(this.waiting.moveUnits);
+        assertNotUndefined(this.waiting.endMovingPhase);
+        assertNotUndefined(this.waiting.endWaitingPhase);
+        assertNotUndefined(this.waiting.getUpdates);
+        assertNotUndefined(this.waiting.isMoveLegal);           
     }
+    
     
 });
 
