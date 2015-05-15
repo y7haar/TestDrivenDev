3d7d2dcf8e3687412db26c302925f38420e9fdac
Al3xCore <alex-kern@online.de>
2015-05-15 04:25:14 +0200

    added testcases, testing if relevant functions of all stats are implemented correctly (not finished yet)

--- test/client/stateTest.js
+++ test/client/stateTest.js
@@ -16,9 +16,11 @@ TestCase("stateTest", {
         // map for testing
         this.map1 = new tddjs.client.map.map();
         this.player1 = new tddjs.client.player();
+        this.player1.setName('Peter');
         this.player2 = new tddjs.client.player();
+        this.player2.setName('Hanswurst');
         
-        //Continent1-------------------------------------
+        //Continent1--- EUROPA ----------------------
         this.continent1 = new tddjs.client.map.continent();
         this.continent1.setName("Europa");
         
@@ -32,13 +34,13 @@ TestCase("stateTest", {
         
         this.c3 = new tddjs.client.map.country();
         this.c3.setName("Country3");
-        this.c3.setOwner(this.player1);
+        this.c3.setOwner(this.player2);
         
         this.continent1.addCountry(this.c1);
         this.continent1.addCountry(this.c2);
         this.continent1.addCountry(this.c3);
         
-        //Continent2---------------------------
+        //Continent2--- ASIEN ------------------------
         this.continent2 = new tddjs.client.map.continent();
         this.continent2.setName("Asien");
         this.c4 = new tddjs.client.map.country();
@@ -56,7 +58,8 @@ TestCase("stateTest", {
         this.continent2.addCountry(this.c4);
         this.continent2.addCountry(this.c5);
         this.continent2.addCountry(this.c6);
-        //-------------------------------------        
+        //Borders-------------------------------
+        this.c1.addBorder(c2);
     }, 
     tearDown: function ()
     {
@@ -135,11 +138,111 @@ TestCase("stateTest", {
         assertFunction(this.placing.endPlacingPhase);
         assertFunction(this.placing.getUpdates);
         assertFunction(this.placing.isMoveLegal);
-        
-        
-        
-     }
     
+     },
+     "test attacking state should implement relevant functions": function () {
+        assertFunction(this.attacking.attack);
+        assertFunction(this.attacking.endAttackingPhase);
+        assertFunction(this.attacking.getUpdates);
+        assertFunction(this.attacking.isMoveLegal);
+        
+        var validMove = {
+            type: 'attack',
+            from: {
+                player: 'Peter',
+                continent: 'Europa',
+                country: 'Country1'
+            },
+            to: {
+                player: 'Hanswurst',
+                continent: 'Europa',
+                country: 'Country2'
+            }
+        };
+        var wrongOwnerMove = {
+            type: 'attack',
+            from: {
+                player: 'Peter',
+                continent: 'Europa',
+                country: 'Country1'
+            },
+            to: {
+                player: 'PeterLustig',
+                continent: 'Europa',
+                country: 'Country2'
+            }
+        };        
+        var wrongCountryMove = {
+            type: 'attack',
+            from: {
+                player: 'Peter',
+                continent: 'Europa',
+                country: 'Country1'
+            },
+            to: {
+                player: 'Hanswurst',
+                continent: 'Europa',
+                country: 'Country4'
+            }
+        };
+        var wrongBorderMove = {
+            type: 'attack',
+            from: {
+                player: 'Peter',
+                continent: 'Europa',
+                country: 'Country1'
+            },
+            to: {
+                player: 'Hanswurst',
+                continent: 'Europa',
+                country: 'Country3'
+            }
+        };
+        var wrongContinentMove = {
+            type: 'attack',
+            from: {
+                player: 'Peter',
+                continent: 'Europa',
+                country: 'Country1'
+            },
+            to: {
+                player: 'Hanswurst',
+                continent: 'Asien',
+                country: 'Country3'
+            }
+        };
+        var wrongTypeMove = {
+            type: 'winGame',
+            from: {
+                player: 'Peter',
+                continent: 'Europa',
+                country: 'Country1'
+            },
+            to: {
+                player: 'Hanswurst',
+                continent: 'Asien',
+                country: 'Country3'
+            }
+        };
+
+        assertTrue(this.attacking.isMoveLegal(this.map1), validMove);
+        assertFalse(this.attacking.isMoveLegal(this.map1), wrongBorderMove);
+        assertFalse(this.attacking.isMoveLegal(this.map1), wrongContinentMove);
+        assertFalse(this.attacking.isMoveLegal(this.map1), wrongCountryMove);
+        assertFalse(this.attacking.isMoveLegal(this.map1), wrongOwnerMove);
+        assertFalse(this.attacking.isMoveLegal(this.map1), wrongTypeMove);
+    },
+    "test moving state should implement relevant functions": function () {
+        assertFunction(this.moving.moveUnits);
+        assertFunction(this.moving.endMovingPhase);
+        assertFunction(this.moving.getUpdates);
+        assertFunction(this.moving.isMoveLegal);
+
+    },    
+    "test waiting state should implement relevant functions": function () {
+        assertFunction(this.waiting.endWaitingPhase);
+        assertFunction(this.waiting.getUpdates);
+    }
     
 });
 
