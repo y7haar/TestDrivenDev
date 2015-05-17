52208358bdf7ac85fe649e76faf14ebddc2797ed
Al3xCore <alex-kern@online.de>
2015-05-17 22:28:57 +0200

    attackingState now can validate Moves and return if they are valid or not.

--- src/client/gameStates.js
+++ src/client/gameStates.js
@@ -25,60 +25,59 @@ state.prototype.toString = null;
 
 function placingState()
 {
-   function placeUnits(currentMap, unitCount, move, url)
-   {
-       if(typeof url !== 'string')
-           throw new TypeError("given url is not a String");
-       if(!isMoveLegal(currentMap, unitCount, move))
-           return false;
-       // ajax POST to server with the move, server should validate the move then trigger eventSource event       
-   }
-   
-   function endPhase()
-   {
-       //ajax Post to server that player want to end this Phase 
-   }
-   function toString()
-   {
-       return "placingState";
-   }
-   
-   function isMoveLegal(currentMap, unitCount, move)
-   {
-       if(!(currentMap instanceof tddjs.client.map.map))
-           throw new TypeError("given Map is not instance of Map");
-       
-       if(isNaN(unitCount))
-           throw new TypeError("given unitCount is not a Number");
-       
-       if(typeof move !== 'object')
-           throw new TypeError("given Move is not in right Format");
-       
-       if(move.type !== 'placing')
-           return false;
-    
-       if(move.unitCount > unitCount)
-           return false;
- 
-       //test if Continent exists 
-       if(! currentMap.hasContinent(move.continent))
-           return false;
-       else
-       {
-           //test if Country not exists on the Continent in moveObject 
-           if(! currentMap.getContinent(move.continent).hasCountryByName(move.country))
-               return false;
-           //test if the player dont own the Country    
-           else if(currentMap.getContinent(move.continent).getCountry(move.country).getOwner().getName() !== move.player)
-               return false;
-           else return true;
-       }
-       
-   }   
-   this.placeUnits = placeUnits;
-   this.isMoveLegal = isMoveLegal;
-   this.toString = toString;
-   this.endPhase = endPhase;
+    function placeUnits(currentMap, unitCount, move, url)
+    {
+        if (typeof url !== 'string')
+            throw new TypeError("given url is not a String");
+        if (!isMoveLegal(currentMap, unitCount, move))
+            return false;
+        // ajax POST to server with the move, server should validate the move then trigger eventSource event       
+    }
+
+    function endPhase()
+    {
+        //ajax Post to server that player want to end this Phase 
+    }
+    function toString()
+    {
+        return "placingState";
+    }
+
+    function isMoveLegal(currentMap, unitCount, move)
+    {
+        if (!(currentMap instanceof tddjs.client.map.map))
+            throw new TypeError("given Map is not instance of Map");
+
+        if (isNaN(unitCount))
+            throw new TypeError("given unitCount is not a Number");
+
+        if (typeof move !== 'object')
+            throw new TypeError("given Move is not in right Format");
+
+        if (move.type !== 'placing')
+            return false;
+
+        if (move.unitCount > unitCount)
+            return false;
+
+        //test if Continent exists 
+        if (!currentMap.hasContinent(move.continent))
+            return false;
+
+        //test if Country not exists on the Continent in moveObject 
+        if (!currentMap.getContinent(move.continent).hasCountryByName(move.country))
+            return false;
+        //test if the player dont own the Country    
+        if (currentMap.getContinent(move.continent).getCountry(move.country).getOwner().getName() !== move.player)
+            return false;
+        // if passed till here move is Valid
+        return true;
+
+    }
+    this.placeUnits = placeUnits;
+    this.isMoveLegal = isMoveLegal;
+    this.toString = toString;
+    this.endPhase = endPhase;
 }
 placingState.prototype = new state();
 placingState.prototype.constructor = placingState;
@@ -89,22 +88,54 @@ function attackingState()
     {
         
     }
+    
     function isMoveLegal(currentMap, move)
     {
-        if(!(currentMap instanceof tddjs.client.map.map))
-           throw new TypeError("given Map is not instance of Map");
-       
-       if(typeof move !== 'object')
-           throw new TypeError("given Move is not in right Format");
-       
-       if(move.type !== 'attack')
-           return false;
+        if (!(currentMap instanceof tddjs.client.map.map))
+            throw new TypeError("given Map is not instance of Map");
+
+        if (typeof move !== 'object')
+            throw new TypeError("given Move is not in right Format");
+
+        if (move.type !== 'attack')
+            return false;
+
+        var attacker = move.from;
+        var defender = move.to;
+
+        //attacker tests
+        if (!currentMap.hasContinent(attacker.continent))
+            return false;
+        if (!currentMap.getContinent(attacker.continent).hasCountryByName(attacker.country))
+            return false;
+        if (currentMap.getContinent(attacker.continent).getCountry(attacker.country).getOwner().getName() !== attacker.player)
+            return false;
+
+        //defender tests
+        if (!currentMap.hasContinent(defender.continent))
+            return false;
+        if (!currentMap.getContinent(defender.continent).hasCountryByName(defender.country))
+            return false;
+        if (currentMap.getContinent(defender.continent).getCountry(defender.country).getOwner().getName() !== defender.player)
+            return false;
+
+        //border tests
+        var attackerCountry = currentMap.getContinent(attacker.continent).getCountry(attacker.country);
+        var defenderCountry = currentMap.getContinent(defender.continent).getCountry(defender.country);
+
+        if (!(attackerCountry.borders(defenderCountry) && defenderCountry.borders(attackerCountry)))
+            return false;
         
+        // if passed till here move is Valid
+        return true;
+
     }
+    
     function toString()
     {
         return "attackingState";
     }
+    
     function endPhase()
     {
         
