4dae7f2cf1ea26bcdecc9d676b9de178ebbefefe
y7haar <y7haar@gmail.com>
2015-05-18 21:33:12 +0200

    Removed all test cases from ajax facade stub because of bad structure

--- test/client/ajaxTest.js
+++ test/client/ajaxTest.js
@@ -2,60 +2,13 @@
  *  TestCases for AJAX Facade Stub
  */
 
-/* 
- *  Testcases for Ajax Stub
- */
-
 TestCase("AjaxFacadeStubTest", {
     setUp: function () {
-        this.ajax = new tddjs.stubs.ajax();
+
     }, 
     tearDown: function ()
     {
-        delete this.ajax;
-    },
-    
-    "test ajax object should not be undefined and instanceof ajax": function () {  
-        assertObject(this.ajax);
-        assertTrue(this.ajax instanceof tddjs.stubs.ajax);
-    },
-    
-    "test ajax facade should hold an instance of xhr object": function () {  
-        var xhr = this.ajax.getXmlHttpRequest();
-        
-        assertObject(xhr);
-        assertTrue(xhr instanceof tddjs.stubs.ajax.xmlHttpRequest);
-    },
-    
-    "test ajax facade should have a GET and POST method to perform requests and a method to set / get headers": function () {  
-        assertFunction(this.ajax.get);
-        assertFunction(this.ajax.post);
-        assertFunction(this.ajax.setRequestHeader);
-        assertFunction(this.ajax.getRequestHeader);
-    },
-    
-    "test ajax facade should call xhr open method when GET or POST request is called": function () {  
-        var xhr = this.ajax.getXmlHttpRequest();
-        
-        this.ajax.get();
-        assertTrue(xhr.isOpenCalled());
-        
-        this.ajax.post();
-        assertTrue(xhr.isOpenCalled());
- 
-    },
-    
-    "test ajax facade should call xhr setHeader method and set correct headers if headers are setted": function () {  
-        var xhr = this.ajax.getXmlHttpRequest();
-        
-        this.ajax.setRequestHeader("Content-Type", "text/plain");
-        assertTrue(xhr.isSetRequestHeaderCalled());
-        assertEquals(this.ajax.getRequestHeader("Content-Type"), xhr.getRequestHeader("Content-Type"));
-        
-        this.ajax.setRequestHeader("Content-Type", "application/json");
-        assertTrue(xhr.isSetRequestHeaderCalled());
-        assertEquals(this.ajax.getRequestHeader("Content-Type"), xhr.getRequestHeader("Content-Type"));
- 
+
     }
 });
 
