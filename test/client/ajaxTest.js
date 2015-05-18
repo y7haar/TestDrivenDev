32664a2ffec247ef206910454ab602b52b120236
y7haar <y7haar@gmail.com>
2015-05-18 21:53:06 +0200

    Refactored test in ajax facade

--- test/client/ajaxTest.js
+++ test/client/ajaxTest.js
@@ -11,12 +11,13 @@ TestCase("AjaxFacadeStubTest", {
 
     },
     
-    "test facade should provide a create method to get a xhr object": function () {  
+    "test facade should provide a create method to get a xhr object with important functions": function () {  
         assertFunction(tddjs.stubs.ajax.create);
-        
         var xhr = tddjs.stubs.ajax.create();
         
-        assertTrue(xhr instanceof tddjs.stubs.ajax.xmlHttpRequest);
+        assertFunction(xhr.open);
+        assertFunction(xhr.send);
+        assertFunction(xhr.setRequestHeader);
     },
 });
 
