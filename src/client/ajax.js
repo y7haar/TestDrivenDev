099be7cbd5f09b46f1451076de7e81b5ed01a9d9
y7haar <y7haar@gmail.com>
2015-05-18 21:33:43 +0200

    Removed all functions from ajax facade stub because of bad structure

--- src/client/ajax.js
+++ src/client/ajax.js
@@ -7,38 +7,6 @@ tddjs.namespace("stubs").ajax = ajax;
 
 function ajax()
 {
-    var _xhr = new tddjs.stubs.ajax.xmlHttpRequest();
-    
-    function getXmlHttpRequest()
-    {
-        return _xhr;
-    }
-    
-    function get()
-    {
-        _xhr.open("GET", "/url", true);
-    }
-    
-    function post()
-    {
-        _xhr.open("POST", "/url", true);
-    }
-    
-    function setRequestHeader(header, value)
-    {
-        _xhr.setRequestHeader(header, value);
-    }
-    
-    function getRequestHeader(header)
-    {
-        return _xhr.getRequestHeader(header);
-    }
-    
-    this.getXmlHttpRequest = getXmlHttpRequest;
-    this.get = get;
-    this.post = post;
-    this.setRequestHeader = setRequestHeader;
-    this.getRequestHeader = getRequestHeader;
     
 };
 
