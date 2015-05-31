f0fe1e219c3984ba84cb3b15c4bae7da2b99c059
y7haar <y7haar@gmail.com>
2015-05-31 18:43:57 +0200

    Created Files for data exchange with lobbies

---
+++ src/client/lobbyRequestController.js
@@ -0,0 +1,7 @@
+/* 
+ *  Source-Code for Lobby Request Controller
+ *  
+ *  This class handles data exchange with the server for the lobby system
+ */
+
+
---
+++ test/client/lobbyRequestControllerTest.js
@@ -0,0 +1,6 @@
+/* 
+ *  Test cases for Lobby Requests
+ */
+
+
+
