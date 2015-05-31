a1ab279f5d4b2693d25b996e39b2e4b18bd40d57
y7haar <y7haar@gmail.com>
2015-05-31 18:48:56 +0200

    Added test for object creation in lobbyRequestController

--- test/client/lobbyRequestControllerTest.js
+++ test/client/lobbyRequestControllerTest.js
@@ -2,5 +2,17 @@
  *  Test cases for Lobby Requests
  */
 
-
-
+TestCase("LobbyRequestControllerTest", {
+    setUp: function () {
+        this.lobbyRequestController = new tddjs.client.controller.lobbyRequestController();
+    }, 
+    tearDown: function ()
+    {
+        var ajax = tddjs.stubs.ajax;
+        ajax.create = this.ajaxCreate;
+    },
+    
+    "test lobbyRequestController should not be undefined after constructor call": function () {  
+        assertObject(this.lobbyRequestController);
+    }
+});
