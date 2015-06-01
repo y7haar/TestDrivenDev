951496cf5b4c28bb5c6ce3a38e1191a3ea7fc360
Yannic Siebenhaar <y7haar@gmail.com>
2015-06-02 00:45:20 +0200

    Implemented creation of lobby div in lobbyUi

--- src/client/lobbyUi.js
+++ src/client/lobbyUi.js
@@ -18,7 +18,31 @@ function lobbyUi()
     
     function addLobby(aLobby)
     {
+        var lobbyId = aLobby.id;
+        var lobbyName = aLobby.name;
+        var lobbyMaxPlayers = aLobby.maxPlayers;
+        var lobbyCurrentPlayers = aLobby.players.length;
         
+        var div = document.createElement("div");
+        var table = document.createElement("table");
+        var tr = document.createElement("tr");
+        
+        var td1 = document.createElement("td");
+        var td2 = document.createElement("td");
+        var td3 = document.createElement("td");
+        
+        td1.innerHTML = "#" + lobbyId;
+        td2.innerHTML = lobbyName;
+        td3.innerHTML = "" + lobbyCurrentPlayers + " / " + lobbyMaxPlayers
+        
+        div.appendChild(table);
+        table.appendChild(tr);
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+        
+        var wrapper = document.getElementById("lobbyWrapper");
+        wrapper.appendChild(div);
     }
     
     this.createWrapper = createWrapper;
