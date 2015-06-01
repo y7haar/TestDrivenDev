10ada8ef3b61b0f2c127e3baca434202b205d848
Yannic Siebenhaar <y7haar@gmail.com>
2015-06-01 17:53:11 +0200

    Added test if lobbyUi creates a div wrapper

--- test/client/lobbyUiTest.js
+++ test/client/lobbyUiTest.js
@@ -14,5 +14,21 @@ TestCase("LobbyUiTest", {
     
     "test lobbyUi should not be undefined after constructor call": function () {  
         assertObject(this.lobbyUi);
+    },
+    
+    "test lobbyUi should have a function to create div wrapper": function () {  
+        assertFunction(this.lobbyUi.createWrapper);
+    },
+    
+    // DOM Tests
+    
+    /*:DOC += <div class = "content" id = "content"></div> */
+    "test createWrapper should append the content with correct class": function () {  
+        assertEquals("", document.getElementById("content").innerHTML);
+        
+        this.lobbyUi.createWrapper();
+        
+        assertNotUndefined(document.getElementById("lobbyWrapper"));
+        assertEquals("lobbyWrapper", document.getElementById("lobbyWrapper".className));
     }
 });
