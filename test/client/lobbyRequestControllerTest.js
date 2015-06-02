70a4f74ef553f07e330ca57ba05c7ca7f2c1d624
y7haar <y7haar@gmail.com>
2015-06-02 23:40:04 +0200

    Refactored test case for lobbyRequestController, now uses 2 lobbies

--- test/client/lobbyRequestControllerTest.js
+++ test/client/lobbyRequestControllerTest.js
@@ -13,32 +13,47 @@ TestCase("LobbyRequestControllerTest", {
         
         
        this.lobby1 = new tddjs.server.model.lobby();
-       this.lobbyController = new tddjs.server.controller.lobbyController();
+       this.lobby2 = new tddjs.server.model.lobby();
       
       this.player1 = new tddjs.client.player();
       this.player2 = new tddjs.client.player();
+      this.player3 = new tddjs.client.player();
+      this.player4 = new tddjs.client.player();
       
       this.player1.setId(1);
       this.player2.setId(2);
+      this.player3.setId(3);
+      this.player4.setId(4);
       
       this.player1.setName("P1");
       this.player2.setName("P2");
+      this.player3.setName("P3");
+      this.player4.setName("P4");
       
       this.player1.setColor("#000000");
       this.player2.setColor("#111111");
+      this.player3.setColor("#222222");
+      this.player4.setColor("#333333");
       
       this.lobby1.setId(0);
+      this.lobby2.setId(1);
       
       this.lobby1.setName("L1");
+      this.lobby2.setName("L2");
       
       this.lobby1.addPlayer(this.player1);
       this.lobby1.addPlayer(this.player2);
+      this.lobby2.addPlayer(this.player3);
+      this.lobby2.addPlayer(this.player4);
       
       this.lobby1.setLeader(this.player1);
+      this.lobby2.setLeader(this.player4);
       
       this.lobby1.setMaxPlayers(2);
+      this.lobby2.setMaxPlayers(3);
       
       this.lobbyController.addLobby(this.lobby1);
+      this.lobbyController.addLobby(this.lobby2);
         
     }, 
     tearDown: function ()
@@ -97,27 +112,44 @@ TestCase("LobbyRequestControllerTest", {
 
         assertEquals("", this.wrapper.innerHTML);
         
+        this.lobbyUi.addLobby(JSON.parse(this.lobby1Json));
+        this.lobbyUi.addLobby(JSON.parse(this.lobby2Json));
+        
         var lobbies = this.wrapper.childNodes;
-        assertEquals(1, lobbies.length);
+        assertEquals(2, lobbies.length);
         
         assertTagName("div", lobbies[0]);
+        assertTagName("div", lobbies[1]);
         
         
         var lobbyTable1 = lobbies[0].getElementsByTagName("table")[0];
+        var lobbyTable2 = lobbies[1].getElementsByTagName("table")[0];
         
         assertTagName("table", lobbyTable1);
+        assertTagName("table", lobbyTable2);
         
         assertTagName("tr", lobbyTable1.getElementsByTagName("tr")[0]);
+        assertTagName("tr", lobbyTable2.getElementsByTagName("tr")[0]);
         
         var lobbyTrContent1 = lobbyTable1.getElementsByTagName("tr")[0].childNodes;
+        var lobbyTrContent2 = lobbyTable2.getElementsByTagName("tr")[0].childNodes;
         
         assertTagName("td", lobbyTrContent1[0]);
         assertTagName("td", lobbyTrContent1[1]);
         assertTagName("td", lobbyTrContent1[2]);
-
+        
+        assertTagName("td", lobbyTrContent2[0]);
+        assertTagName("td", lobbyTrContent2[1]);
+        assertTagName("td", lobbyTrContent2[2]);
+        
+        
         assertEquals("#0", lobbyTrContent1[0].innerHTML);
         assertEquals("L1", lobbyTrContent1[1].innerHTML);
         assertEquals("2 / 2", lobbyTrContent1[2].innerHTML);
+        
+        assertEquals("#1", lobbyTrContent2[0].innerHTML);
+        assertEquals("L2", lobbyTrContent2[1].innerHTML);
+        assertEquals("2 / 3", lobbyTrContent2[2].innerHTML);
     },
     
     
