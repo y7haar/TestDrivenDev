3b593413b8a3e6734a281b92b79ba93ce60907fa
y7haar <y7haar@gmail.com>
2015-06-03 20:57:35 +0200

    Removed tests in nameListReader, function will be private

--- test/server/nodeJs/nameListReaderTest.js
+++ test/server/nodeJs/nameListReaderTest.js
@@ -15,14 +15,6 @@ TestCase("NameListReaderTest", {
 
   "test object of nameListReader should not be undefined": function () { 
         assertObject(this.nameListReader);
-  },
-  
-   "test nameListReader should have function to split up string into array": function () { 
-        assertFunction(this.nameListReader.splitList);
-  },
-  
-  "test nameListReader should have getter for nameList": function () { 
-        assertFunction(this.nameListReader.getList);
   }
   
 });
