1e749edbcdb46e618e3e8d1ee09eeebb267f58d0
y7haar <y7haar@gmail.com>
2015-06-04 19:06:06 +0200

    Implemented respondNewLobby method

--- src/server/lobbyResponseController.js
+++ src/server/lobbyResponseController.js
@@ -10,6 +10,26 @@ function lobbyResponseController()
     {
         if(typeof req !== "string")
             throw new TypeError("Request must be string");
+        
+        try
+        {
+            var obj = JSON.parse(req);
+            
+            var player = obj.player;
+            if(typeof player !== "object")
+                throw new Error("Player must be an object");
+            
+            if(typeof player.name !== "string")
+                throw new Error("Name must be a string");
+            
+            if(typeof player.color !== "string")
+                throw new Error("Color must be a string");
+            
+        }
+        catch(e)
+        {
+            throw new Error("JSON is not valid");
+        }
     }
     
     this.respondNewLobby = respondNewLobby;
