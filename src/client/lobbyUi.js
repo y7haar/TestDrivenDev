755ae16269a088cb1658bc6b08d96d63dcae4442
y7haar <y7haar@gmail.com>
2015-06-04 22:02:18 +0200

    Implemented button in lobbyUi

--- src/client/lobbyUi.js
+++ src/client/lobbyUi.js
@@ -36,10 +36,24 @@ function lobbyUi()
         var div = document.createElement("div");
         var table = document.createElement("table");
         var tr = document.createElement("tr");
+        div.className = "lobby";
         
         var td1 = document.createElement("td");
+        td1.className = "lobbyId";
+        
         var td2 = document.createElement("td");
+        td2.className = "lobbyName";
+        
         var td3 = document.createElement("td");
+        td3.className = "lobbyPlayers";
+        
+        var td4 = document.createElement("td");
+        td4.className = "lobbyJoin";
+        
+        var td4Div = document.createElement("div");
+        td4.appendChild(td4Div);
+        
+        td4Div.innerHTML = "Join";
         
         td1.innerHTML = "#" + lobbyId;
         td2.innerHTML = lobbyName;
@@ -50,6 +64,7 @@ function lobbyUi()
         tr.appendChild(td1);
         tr.appendChild(td2);
         tr.appendChild(td3);
+        tr.appendChild(td4);
         
         var wrapper = document.getElementById("lobbyWrapper");
         wrapper.appendChild(div);
@@ -58,6 +73,8 @@ function lobbyUi()
     function showErrorMessage()
     {
         var lobbyWrapper = document.getElementById("lobbyWrapper");
+        lobbyWrapper.innerHTML = "";
+        
         var p = document.createElement("p");
         p.innerHTML = "Error: Could not get data";
         
