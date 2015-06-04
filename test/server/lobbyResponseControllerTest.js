9b89790e55a332c9c304a12b63e8c44835a4e1a6
y7haar <y7haar@gmail.com>
2015-06-04 19:05:44 +0200

    Fixed testcase

--- test/server/lobbyResponseControllerTest.js
+++ test/server/lobbyResponseControllerTest.js
@@ -28,50 +28,43 @@ TestCase("LobbyResponseControllerTest", {
         assertException(function() { controller.respondNewLobby(3); }, "TypeError");
     },
     
-    "test respondNewLobby should throw Error if given data is not valid": function () {  
+    //respondNewLobby should throw Error if given data is not valid
+    "test ": function () {  
         var controller = this.lobbyResponseController;
         
         var req = "";
-        assertException(controller.respondNewLobby(req), "Error");
+        assertException(function() { controller.respondNewLobby(req); }, "Error");
         
         req = "{}";
-        assertException(controller.respondNewLobby(req), "Error");
-        
-        var obj = { type: "bla" };
-        req = JSON.stringify(obj);
-        assertException(controller.respondNewLobby(req), "Error");
-        
-        var obj = { type: "create" };
-        req = JSON.stringify(obj);
-        assertException(controller.respondNewLobby(req), "Error");
+        assertException(function() { controller.respondNewLobby(req); }, "Error");
         
          var obj = { type: "create" };
         req = JSON.stringify(obj);
-        assertException(controller.respondNewLobby(req), "Error");
+        assertException(function() { controller.respondNewLobby(req); }, "Error");
         
         var obj = { type: "create", player: 6 };
         req = JSON.stringify(obj);
-        assertException(controller.respondNewLobby(req), "Error");
+        assertException(function() { controller.respondNewLobby(req); }, "Error");
         
         var obj = { type: "create", player: {} };
         req = JSON.stringify(obj);
-        assertException(controller.respondNewLobby(req), "Error");
+        assertException(function() { controller.respondNewLobby(req); }, "Error");
         
          var obj = { type: "create", player: { name: 4 } };
         req = JSON.stringify(obj);
-        assertException(controller.respondNewLobby(req), "Error");
+        assertException(function() { controller.respondNewLobby(req); }, "Error");
         
         var obj = { type: "create", player: { name: "Peter" } };
         req = JSON.stringify(obj);
-        assertException(controller.respondNewLobby(req), "Error");
+        assertException(function() { controller.respondNewLobby(req); }, "Error");
         
         var obj = { type: "create", player: { name: "Peter", color: 3 } };
         req = JSON.stringify(obj);
-        assertException(controller.respondNewLobby(req), "Error");
+        assertException(function() { controller.respondNewLobby(req); }, "Error");
         
         var obj = { type: "create", player: { name: "Peter", color: "#ffffff" } };
         req = JSON.stringify(obj);
-        assertNoException(controller.respondNewLobby(req));
+        assertNoException(function() { controller.respondNewLobby(req); });
     }
 });
 
