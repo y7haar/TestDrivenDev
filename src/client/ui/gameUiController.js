bae83f3f8b316e56f15915cd77318f6d25247c0b
René Lottes <rene.lottes@hof-university.de>
2015-06-10 12:31:42 +0200

    TestCase for gameUiController

--- src/client/ui/gameUiController.js
+++ src/client/ui/gameUiController.js
@@ -7,4 +7,19 @@ tddjs.namespace("client.ui").gameUiController = gameUiController;
 
 function gameUiController(){
     
+    var _btn =[];
+    
+    function addButton(btn){
+        
+        _btn.push(btn);
+    }
+    function getButtons(){
+        return _btn;
+    }
+    
+    
+    
+    
+    this.addButton = addButton;
+    this.getButtons = getButtons;
 }
