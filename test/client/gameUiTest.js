98b0005d2e4654ed97d6a08d30e9bfedfd004bb7
René Lottes <rene.lottes@hof-university.de>
2015-06-10 08:55:51 +0200

    erste Tests für UI schnittstelle

--- test/client/gameUiTest.js
+++ test/client/gameUiTest.js
@@ -2,4 +2,26 @@
  *  Test cases for the Game UI
  */
 
+function gameUiSetup()
+{
+    //this.gameUi = new tddjs.client.ui.gameUiController();
+    this.button = new tddjs.client.ui.button(170,65,"button",null);
+}
 
+function gameUiTeardown()
+{
+    //delete this.gameUi;
+}
+
+TestCase("GameUiTest", {
+    setUp: gameUiSetup,
+    tearDown: gameUiTeardown,
+    
+    "test gameUi should not be undefined after constructor call": function () {  
+        assertObject(this.gameUi);
+    },
+    
+    "test gameUi button": function () {  
+        assert(this.button.isCoordOnButton(170,65));
+    }
+});
