37d1d91d287e68a2652261d83c3d4d125f512dc1
René Lottes <rene.lottes@hof-university.de>
2015-06-10 09:30:58 +0200

    gameUiTest button coord tests

--- test/client/gameUiTest.js
+++ test/client/gameUiTest.js
@@ -7,9 +7,14 @@ function gameUiSetup()
     /*:DOC += <canvas id="game" width="700" height="500"></canvas> */
     
     this.canvas = document.getElementById('game');
+    assertObject(this.canvas);
     this.ctx = this.canvas.getContext("2d");
+    assertObject(this.ctx);
+    
     this.gameUi = null;//new tddjs.client.ui.gameUiController();
-    this.button = new tddjs.client.ui.button(170,65,"button",ctx);
+    this.button = new tddjs.client.ui.button(170,65,"button",this.ctx);
+    this.buttonW = this.ctx.measureText("button").width;
+    this.buttonH = this.ctx.measureText("button").height;
 }
 
 function gameUiTeardown()
@@ -28,7 +33,10 @@ TestCase("GameUiTest", {
         assertObject(this.gameUi);
     },
     
-    "test gameUi button": function () {  
-        assert(this.button.isCoordOnButton(160,65));
+    "test gameUi button: coord should be on the Button": function () {        
+        assert(this.button.isCoordOnButton(170,65));
+        assert(this.button.isCoordOnButton(this.buttonW,65));
+        assert(this.button.isCoordOnButton(170,this.buttonH));
+        assert(this.button.isCoordOnButton(this.buttonW,this.buttonH));
     }
 });
