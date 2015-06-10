d62f7fbedfa2a7bf6bc5f9d57621719c979580f8
René Lottes <rene.lottes@hof-university.de>
2015-06-10 12:34:11 +0200

    gameUiController can now store Buttons

--- src/client/ui/gameUiController.js
+++ src/client/ui/gameUiController.js
@@ -10,7 +10,10 @@ function gameUiController(){
     var _btn =[];
     
     function addButton(btn){
-        
+        if(arguments.length < 1)
+            throw new Error("Wrong parametercount!");
+        if(!(aContinent instanceof tddjs.client.ui.button))
+            throw new TypeError("Parameter ist not instance of Button");
         _btn.push(btn);
     }
     function getButtons(){
