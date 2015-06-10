37202eba44bf58ece7663ee580cf2c940cae8690
Al3xCore <alex-kern@online.de>
2015-06-11 00:03:16 +0200

    gameLoopController  now implementes getMap , getPlayer, getUrl  which return the requestet member value

--- src/client/gameLoopController.js
+++ src/client/gameLoopController.js
@@ -5,16 +5,35 @@
 
 tddjs.namespace("client").gameLoopController = gameLoopController;
 
-function gameLoopController()
+function gameLoopController(aMap, aPlayer, aUrl)
 {
+    setMap(aMap);
+    setPlayer(aPlayer);
+    setUrl(aUrl);
+    
+    
     var _player;
     var _map;
+    var _url;
     var _currentState = new tddjs.client.waitingState();
+  
+    
+    function setUrl(aUrl)
+    {
+        if(typeof aUrl !== 'string')
+            throw new TypeError("Url ist not a valid Url-String");
+        _url = aUrl;
+    }
+    
+    function getUrl()
+    {
+        return _url;
+    }
     
     function setMap(aMap)
     {
         if(!(aMap instanceof tddjs.client.map.map))
-            throw new TypeError("Parameter ist not instance of Map");
+            throw new TypeError("Map ist not instance of Map");
         _map = aMap;
     }
     
@@ -26,7 +45,7 @@ function gameLoopController()
     function setPlayer(aPlayer)
     {
         if(!(aPlayer instanceof tddjs.client.player))
-            throw new TypeError("Parameter ist not instance of Player");
+            throw new TypeError("Player ist not instance of Player");
         _player = aPlayer;
     }
     
@@ -43,10 +62,9 @@ function gameLoopController()
     function isMoveLegal()
     {
     }
-
-    this.setMap = setMap;
+    
+    this.getUrl = getUrl;
     this.getMap = getMap;
-    this.setPlayer = setPlayer;
     this.getPlayer = getPlayer;    
     this.getStateName = getStateName;
     this.isMoveLegal = isMoveLegal;
