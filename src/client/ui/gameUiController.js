1e77701c10631d78cc6dab155b4b7addc86199d5
René Lottes <rene.lottes@hof-university.de>
2015-06-10 14:33:55 +0200

    typo

--- src/client/ui/gameUiController.js
+++ src/client/ui/gameUiController.js
@@ -15,7 +15,7 @@ function gameUiController(aGLC,aCtx){
     }
     
     var _btn =[];
-    var _countrys =[];
+    var _countries =[];
     var _gridMap;
     
     function init(){
@@ -34,22 +34,22 @@ function gameUiController(aGLC,aCtx){
     }
     
     
-    
+    // <editor-fold defaultstate="collapsed" desc="Map-functions">
     function _initMap(aGridMap){
         _gridMap = aGridMap;
         for(x=0;x<_gridMap.cellGrid.length;x++){
             for(y=0;y<_gridMap.cellGrid[0].length;y++){
-                if(_countrys.indexOf(_gridMap.cellGrid[x][y]) === -1)
-                    _countrys.push(_gridMap.cellGrid[x][y]);
+                if(_countries.indexOf(_gridMap.cellGrid[x][y]) === -1)
+                    _countries.push(_gridMap.cellGrid[x][y]);
             }
         }
     }
     
-    function _extendCountrys(){
-        if(_countrys.length === 0)
+    function _extendCountries(){
+        if(_countries.length === 0)
             throw new Error("Call _initMap first!");
-        for (var i=0; i<_countrys.length; ++i)
-            _countrys[i]._isOnCoord=_isOnCoord;
+        for (var i=0; i<_countries.length; ++i)
+            _countries[i]._isOnCoord=_isOnCoord;
     }
     // <editor-fold defaultstate="collapsed" desc="Country helper">
     function _isOnCoord(x,y){
@@ -58,12 +58,12 @@ function gameUiController(aGLC,aCtx){
     }
     // </editor-fold>
     
-    function _getCountrys(){
-        if(_countrys.length === 0)
+    function _getCountries(){
+        if(_countries.length === 0)
             throw new Error("Call _initMap first!");
-        return _countrys;
+        return _countries;
     }
-    
+    // </editor-fold>
     
     this.init = init;
     this.addButton = addButton;
@@ -71,8 +71,8 @@ function gameUiController(aGLC,aCtx){
     
     //private
     this._initMap = _initMap;
-    this._getCountrys = _getCountrys;
-    this._extendCountrys = _extendCountrys;
+    this._getCountries = _getCountries;
+    this._extendCountries = _extendCountries;
     
     //helper
 }
