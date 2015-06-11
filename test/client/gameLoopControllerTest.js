58a14852e12c9f4ce236599a965d220e4f6e20e1
Al3xCore <alex-kern@online.de>
2015-06-11 03:57:15 +0200

    gameLoopTests:  	test if gameLoop hold two arrays  one of Messages to the Server and  on of Messages from the server, arrays should be empty at beginning and increase if the server send or recives msgs.

--- test/client/gameLoopControllerTest.js
+++ test/client/gameLoopControllerTest.js
@@ -183,7 +183,7 @@ TestCase("GameLoopControllerTests", {
 });
 
 
-TestCase("GameLoopEventHandlerTests", {
+TestCase("GameLoopCommunicationTests", {
     setUp: function () {             
         this.map = generateMap();      
         this.player1 = new tddjs.client.player();
@@ -204,11 +204,36 @@ TestCase("GameLoopEventHandlerTests", {
         this.sandbox.restore();
         this.sandbox = null;
     },
+    "test gameLoop should hold a array of all messages from server": function () {
+        assertNotUndefined(this.gameLoop.fromServerLogs);
+    },
+    "test gameLoop.fromServerLogs should be empty at init": function () {
+        assertEquals(0,this.gameLoop.fromServerLogs.length);
+    },
+    "test gameLoop.fromServerLogs should increase if the server send a message": function () {
+        assertEquals(0,this.gameLoop.fromServerLogs.length);
+        this.sandbox.server[this.url].sendMessage(0,"changetowaiting",{data:"change to waiting-State"});
+        assertEquals(1,this.gameLoop.fromServerLogs.length);
+        this.sandbox.server[this.url].sendMessage(0,"changetowaiting",{data:"change to waiting-State"});
+        assertEquals(2,this.gameLoop.fromServerLogs.length);
+    },
+    "test gameLoop.fromServerLogs saved message and sended msg should be same": function () {
+        var msg = {data:"change to waiting-State"};
+        this.sandbox.server[this.url].sendMessage(0,"changetowaiting",msg);
+        assertEquals(1,this.gameLoop.fromServerLogs.length);
+        assertSame(msg, this.gameLoop.fromServerLogs[0]);
+    },
+    "test gameLoop should hold a array of all messages to server": function () {
+        assertNotUndefined(this.gameLoop.toServerLogs);
+    },
+    "test gameLoop.toServerLogs should be empty at init": function () {
+        assertEquals(0,this.gameLoop.toServerLogs.length);
+    },
     "test State should change to attacking state if server trigger changeToAttacking event": function () {        
         assertEquals("waitingState", this.gameLoop.getStateName());
         assertTrue(this.gameLoop.currentState instanceof tddjs.client.waitingState);
          
-        this.sandbox.server[this.url].sendMessage(0,"onchangetoattacking",{data:"change to Attacking-State"});
+        this.sandbox.server[this.url].sendMessage(0,"changetoattacking",{data:"change to Attacking-State"});
          
         assertEquals("attackingState", this.gameLoop.getStateName());
         assertTrue(this.gameLoop.currentState instanceof tddjs.client.attackingState);
@@ -217,20 +242,19 @@ TestCase("GameLoopEventHandlerTests", {
         assertEquals("waitingState", this.gameLoop.getStateName());
         assertTrue(this.gameLoop.currentState instanceof tddjs.client.waitingState);
          
-        this.sandbox.server[this.url].sendMessage(0,"onchangetoplacing",{data:"change to placing-State"});
+        this.sandbox.server[this.url].sendMessage(0,"changetoplacing",{data:"change to placing-State"});
          
         assertEquals("placingState", this.gameLoop.getStateName());
         assertTrue(this.gameLoop.currentState instanceof tddjs.client.placingState);
     },
     "test State should change to waiting state if server trigger changeToWaiting event": function () {
-        this.sandbox.server[this.url].sendMessage(0,"onchangetoplacing",{data:"change to placing-State"});
+        this.sandbox.server[this.url].sendMessage(0,"changetoplacing",{data:"change to placing-State"});
         assertEquals("placingState", this.gameLoop.getStateName());
         assertTrue(this.gameLoop.currentState instanceof tddjs.client.placingState);
         
-        this.sandbox.server[this.url].sendMessage(0,"onchangetowaiting",{data:"change to waiting-State"});
+        this.sandbox.server[this.url].sendMessage(0,"changetowaiting",{data:"change to waiting-State"});
         assertEquals("waitingState", this.gameLoop.getStateName());
-        assertTrue(this.gameLoop.currentState instanceof tddjs.client.waitingState);
-         
+        assertTrue(this.gameLoop.currentState instanceof tddjs.client.waitingState);         
      
     }
     
