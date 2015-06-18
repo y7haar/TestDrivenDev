05a609062d49f409301bd0dea6ce3a75d37fa691
y7haar <y7haar@gmail.com>
2015-06-19 01:03:44 +0200

    Added test file for lobbyEventSourceController

---
+++ test/client/lobbyEventSourceControllerTest.js
@@ -0,0 +1,20 @@
+/* 
+ *  Testcases for LobbyEventSourceController
+ *  
+ *  This class handles data exchange with the Server via Event Source
+ *  
+ */
+
+TestCase("LobbyEventSourceControllerTest", {
+    setUp: function() {
+        this.lobbyEventSourceController = new tddjs.client.controller.eventSourceController();
+
+    },
+    tearDown: function(){
+    },
+    
+    "test LobbyEventSourceController should not be undefined after constructor call": function() {
+        assertObject(this.lobbyEventSourceController);
+    }
+    
+    });
