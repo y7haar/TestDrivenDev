033cf096aa96a7bfa4f2fb407501842fa3be0920
y7haar <y7haar@gmail.com>
2015-06-19 01:06:33 +0200

    Added file for LobbyEventSourceController

---
+++ src/client/lobbyEventSourceController.js
@@ -0,0 +1,15 @@
+/* 
+ *  Source-Code for LobbyEventSourceController
+ *  
+ *   This class handles data exchange with the server via EventSource
+ */
+
+tddjs.namespace("client.controller").lobbyEventSourceController = lobbyEventSourceController;
+
+function lobbyEventSourceController()
+{
+}
+
+
+
+
