ef79a54eee55212b943f3134565ac784bc55d5eb
y7haar <y7haar@gmail.com>
2015-06-19 01:58:55 +0200

    Updated lobbyEventSourceController Implemented setter / getter for lobby Implemented setter for lonbbyUi Implemented method to establish EvtSource connection

--- src/client/lobbyEventSourceController.js
+++ src/client/lobbyEventSourceController.js
@@ -8,23 +8,39 @@ tddjs.namespace("client.controller").lobbyEventSourceController = lobbyEventSour
 
 function lobbyEventSourceController()
 {
+    var _lobby;
+    var _lobbyUi;
+    var _eventSource;
+    
     function establishConnection()
     {
-        
+        _eventSource = new EventSource(BASE_URL + "lobbies/1");
     }
     
-    function setLobby()
+    function setLobby(aLobby)
     {
+        if(! (aLobby instanceof tddjs.client.model.lobby))
+            throw new TypeError("Lobby must be of Type Lobby");
         
+        _lobby = aLobby;
     }
     
     function getLobby()
     {
+        return _lobby;
+    }
+    
+    function setLobbyUi(aLobbyUi)
+    {
+         if(! (aLobbyUi instanceof tddjs.client.ui.lobbyUi))
+            throw new TypeError("LobbyUi must be of Type LobbyUi");
         
+        _lobbyUi = aLobbyUi;
     }
     
     this.establishConnection= establishConnection;
     this.setLobby = setLobby;
     this.getLobby = getLobby;
+    this.setLobbyUi = setLobbyUi;
     
 }
