21ab250580044f73fa40925756a438aa77ecd9a4
y7haar <y7haar@gmail.com>
2015-06-19 03:00:19 +0200

    Implemented method addEventListeners in controller

--- src/client/lobbyEventSourceController.js
+++ src/client/lobbyEventSourceController.js
@@ -11,36 +11,60 @@ function lobbyEventSourceController()
     var _lobby;
     var _lobbyUi;
     var _eventSource;
-    
+
     function establishConnection()
     {
-        _eventSource = new EventSource(BASE_URL + "lobbies/1");
+        if(! (_lobby instanceof tddjs.client.model.lobby))
+            throw new Error("Lobby must be setted before doing a request");
+        
+        _eventSource = new EventSource(BASE_URL + "lobbies/" + _lobby.getId());
+       
+        this.addEventListeners();
     }
     
+    function addEventListeners()
+    {
+        _eventSource.addEventListener("colorchange", oncolorchange);
+    }
+
     function setLobby(aLobby)
     {
-        if(! (aLobby instanceof tddjs.client.model.lobby))
+        if (!(aLobby instanceof tddjs.client.model.lobby))
             throw new TypeError("Lobby must be of Type Lobby");
-        
+
         _lobby = aLobby;
     }
-    
+
     function getLobby()
     {
         return _lobby;
     }
-    
+
     function setLobbyUi(aLobbyUi)
     {
-         if(! (aLobbyUi instanceof tddjs.client.ui.lobbyUi))
+        if (!(aLobbyUi instanceof tddjs.client.ui.lobbyUi))
             throw new TypeError("LobbyUi must be of Type LobbyUi");
-        
+
         _lobbyUi = aLobbyUi;
     }
-    
-    this.establishConnection= establishConnection;
+
+
+
+    function oncolorchange()
+    {
+
+    }
+
+    this.addEventListeners = addEventListeners;
+    this.establishConnection = establishConnection;
     this.setLobby = setLobby;
     this.getLobby = getLobby;
     this.setLobbyUi = setLobbyUi;
     
+    Object.defineProperty(this, 'eventSource', {
+        get: function () {
+            return _eventSource;
+        }
+    });
+
 }
