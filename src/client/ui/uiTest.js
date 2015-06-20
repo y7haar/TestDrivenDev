ca694ee7e7bcf61c53084837fbafb75ffb414a54
René Lottes <rene.lottes@hof-university.de>
2015-06-20 13:39:22 +0200

    added 'lodingscreen'

--- src/client/ui/uiTest.js
+++ src/client/ui/uiTest.js
@@ -4,26 +4,48 @@
 
 
 var controller;
+var m;
+var m2;
 
 function test(){
-    var m=new tddjs.server.controller.mapGenerator()
+    m=new tddjs.server.controller.mapGenerator()
     m.setGridSize(120,120);
     m.setMaximumCountrySize(200);
     m.setMinimumCountrySize(75);
-     canvas = document.getElementById('game');
+    canvas = document.getElementById('game');
     if (canvas && canvas.getContext) {
        ctx = canvas.getContext("2d");
        if (ctx) {
            controller = new tddjs.client.ui.gameUiController(null,ctx);
-           var m2 = m.generateMap();
-           m2= m.serializeAsJSON(m2);
-           controller._getMap(m2);
-           controller._initMap();
+           
            
            canvas.addEventListener('mousemove', onCanvasMouseMove, false);
-           window.requestAnimationFrame(mainloop);
+           window.requestAnimationFrame(generateMap);
        }
    }
+}
+
+function generateMap(){
+    ctx.strokeStyle="#000";
+    ctx.fillStyle = "#552700";
+    ctx.lineWidth="1";
+    ctx.font = "30px Arial";
+    ctx.fillText("generating map...",10,50);
+    
+    m2 = m.generateMap();
+    m2= m.serializeAsJSON(m2);
+    window.requestAnimationFrame(renderMap);
+}
+function renderMap(){
+    ctx.strokeStyle="#000";
+    ctx.fillStyle = "#552700";
+    ctx.lineWidth="1";
+    ctx.font = "30px Arial";
+    ctx.fillText("render map...",10,80);
+    
+    controller._getMap(m2);
+    controller._initMap();
+    window.requestAnimationFrame(mainloop);
 }
 
 function mainloop(){
