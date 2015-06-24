bd2f118a28377ad359aa7f4cb0a7a964bc026cdf
Al3xCore <alex-kern@online.de>
2015-06-24 04:41:25 +0200

    serverGameLoopController: controller now holds map and init it with null, setMap function is implemented and sets the map to the given value;

--- src/server/serverGameLoopController.js
+++ src/server/serverGameLoopController.js
@@ -12,6 +12,7 @@ else
 
 function gameLoopController()
 {
+    var _map = null;
     var _clients =[];
     var _maxPlayers = -1;
     var _allConnected = false;
@@ -37,6 +38,16 @@ function gameLoopController()
         _maxPlayers = intValue;
     }
     
+    function setMap(aMap)
+    {
+        _map = aMap;
+    }
+    
+    function messageAll()
+    {
+        
+    }
+    
     //test
     Object.defineProperty(this, 'clients', {
         get: function () {
@@ -53,7 +64,18 @@ function gameLoopController()
             return _allConnected;
         }
     });
+    Object.defineProperty(this, 'messageAll', {
+        get: function () {
+            return messageAll;
+        }
+    });
+    Object.defineProperty(this, 'map', {
+        get: function () {
+            return _map;
+        }
+    });
     
+    this.setMap = setMap;
     this.setMaxPlayers = setMaxPlayers;
     this.addClient = addClient;
     
