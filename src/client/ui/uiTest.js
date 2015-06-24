c6772c85eb664af13c48b807ab8068ee2e0233f5
Ralf Heinrich <ralf.heinrich@hof-university.de>
2015-06-24 16:48:24 +0200

    Testweise Namenslisten gestzt für die Namensverteilung von Kontinenten und Ländern

--- src/client/ui/uiTest.js
+++ src/client/ui/uiTest.js
@@ -8,6 +8,7 @@ function startGame(){
     var m;
     var m2;
 
+    //Einstellungen Generator
     m=new tddjs.server.controller.mapGenerator();
     m.setGridSize(150,150);
     m.setMaximumCountrySize(300);
@@ -17,6 +18,32 @@ function startGame(){
     m.setMinimumContinentSize(4);
     m.setMinimumWaterNumber(6);
     m.setMinimumWaterNumber(4);
+    
+    //Namen für Generation Testweise!
+    var countryList = ["Bakaresh", "Rodkaresh", "Bastion Akundur", "Ankh Morpork", "Serandur", "Sturmgipfel",
+                    "Kristallnarbe", "Kristallwüste", "Gewundener Wald", "Piltover", "Zaun", "Demacia", 
+                    "Schicksalsschlucht", "Cavalon", "Haus zu Heltzer", "Zentrum von Kaineng", "Kloster von Shingjea",
+                    "Borlispass", "Gunnars Feste", "Ratasum", "Droknars Schmiede", "Oase Amnoon", 
+                    "Steinkreis von Denravi", "Löwenstein", "Wachturmküste", "Frosttor", "Nolani-Akademie",
+                    "Der große Nordwall", "Fort Ranik", "Erntetempel", "Skyrim", "Kamadan", "Halle der Sonnenspeere",
+                    "Faring", "Valorian", "Sardelek-Sanatorium", "Drascir", "Die schwarze Zitadelle", "Ebonhawk",
+                    "Kluft der Beschwörer", "Abbadons Maul", "Tor des Wahnsinns", "Tor der Angst", "Tor der Schmerzen",
+                    "Tor der Pein", "Vaabi", "Schwarzwasserküste", "Eisminen von Moladune", "Ödland", "Rin",
+                    "Markt von Faible", "Seufzer-Graftschaft", "Dünen der Verzweiflung", "Fels der Weissagung",
+                    "Maguuma-Dschungel", "Zittergipfelgebirge", "Ruinen von Surmia", "Drachenschlund ",
+                    "Pockennarbenebene", "Grooble-Schlucht", "Signalfeuerposten", "Lornarpass", "Arah", 
+                    "Das Trockene Meer", "Auroralichtung", "Ostgrenze", "Tempel der Zeitalter", "Halle der Helden",
+                    "Idylissches Tal", "Noxus", "Ionia", "Shurima", "Platonien", "Gefrorenes Herz",
+                    "Tal des Erzengels", "Trauerschlucht", "Kimberleys Tränen", "Tal des Schreckens", "Minental",
+                    "Khorinis", "Jharkendar", "Irdorath", "Myrtana", "Nordmar", "Varant", "Velen", "Novigrad",
+                    "Skellige", "Weißgarten", "Oxenfurt", "Kaer Morhen", "Wyzima", "Weißlauf", "Flusswald", "Riften",
+                    "Winterfeste", "Morthal", "Windhelm", "Markarth", "Helgen"];
+    m.getCountryNameGenerator().setNameList(countryList);
+    //Kontinentnamen
+    var list = ["Aranonda", "Lorna", "Lors", "Faridon", "Elona", "Cantha", "Tyria",
+                    "Nod", "Rod", "Kryta", "Ascalon", "Elysium", "Braktarien", "Karatie"];
+    m.getContinentNameGenerator().setNameList(list); 
+    
     canvas = document.getElementById('game');
     if (canvas && canvas.getContext) {
        ctx = canvas.getContext("2d");
