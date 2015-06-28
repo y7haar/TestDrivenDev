1701ec25728ae214bc4d1fbbe39581081f3cc6c8
y7haar <y7haar@gmail.com>
2015-06-29 00:47:43 +0200

    Added tests for method broadcastMessage

--- test/server/NEWlobbyResponseControllerTest.js
+++ test/server/NEWlobbyResponseControllerTest.js
@@ -35,6 +35,11 @@ function fakeRes()
     {
         
     };
+    
+    this.write = function(msg)
+    {
+        
+    };
 }
 
 TestCase("LobbyResponseControllerTest", {
@@ -149,4 +154,94 @@ TestCase("LobbyResponseControllerEventSourceTest", {
         sinon.assert.calledOnce(this.resSendStatusSpy);
         sinon.assert.calledWith(this.resSendStatusSpy, 400);
     }
+});
+
+TestCase("LobbyResponseControllerBroadcastTest", {
+    setUp: function() {
+        // Must be changed 
+        this.lrc = new tddjs.server.controller.NEWlobbyResponseController();
+        this.lobbyController = tddjs.server.controller.lobbyController.getInstance();
+        this.lobbyFactory = new tddjs.server.controller.lobbyFactory();
+        
+        this.lobby = this.lobbyFactory.getNewLobby();
+        this.player1 = new tddjs.server.player();
+        this.player1.setToken("1234");
+        
+        this.player2 = new tddjs.server.player();
+        this.player2.setToken("2345");
+        
+        this.player3 = new tddjs.server.player();
+        this.player3.setType("bot");
+        
+        this.lobby.addPlayer(this.player1);
+        this.lobby.addPlayer(this.player2);
+        this.lobby.addPlayer(this.player3);
+        
+        this.res1 = new fakeRes();
+        this.res2 = new fakeRes();
+        
+        this.player1.setResponseObject(this.res1);
+        this.player2.setResponseObject(this.res2);
+        
+        this.lobbyController.addLobby(this.lobby);
+        this.lrc.setLobbyById(this.lobby.getId());
+        
+        // Mocking req and res
+        
+        this.req = new fakeReq();
+        this.res = new fakeRes();
+        
+        this.sandbox = sinon.sandbox.create();
+        
+        this.pl1GetResSpy = this.sandbox.spy(this.player1, "getResponseObject");
+        this.pl2GetResSpy = this.sandbox.spy(this.player2, "getResponseObject");
+        this.pl3GetResSpy = this.sandbox.spy(this.player3, "getResponseObject");
+        
+        this.res1SendSpy = this.sandbox.spy(this.res1, "write");
+        this.res2SendSpy = this.sandbox.spy(this.res2, "write");
+        
+    },
+    tearDown: function()
+    {
+        this.sandbox.restore();
+    },
+    
+     "test lobbyResponseController should have function to broadcast a message to all players in lobby": function() {
+        assertFunction(this.lrc.broadcastMessage);
+    },
+    
+    "test broadcastMessage should throw Exception if message is no object": function() {
+        var controller = this.lrc;
+        assertNoException(function() { controller.broadcastMessage({}, "event"); });
+        
+        assertException(function() { controller.broadcastMessage("asd", "event"); }, "TypeError");
+    },
+    
+    "test broadcastMessage should throw Exception if event is no string": function() {
+        var controller = this.lrc;
+        assertNoException(function() { controller.broadcastMessage({}, "event"); });
+        
+        assertException(function() { controller.broadcastMessage({}, 3); }, "TypeError");
+    },
+    
+    "test broadcastMessage should get all human players res objects and call res.write": function() {
+        sinon.assert.notCalled(this.pl1GetResSpy);
+        sinon.assert.notCalled(this.pl2GetResSpy);
+        sinon.assert.notCalled(this.pl3GetResSpy);
+        
+        sinon.assert.notCalled(this.res1SendSpy);
+        sinon.assert.notCalled(this.res2SendSpy);
+        
+        this.lrc.broadcastMessage({}, "myEvent");
+        
+        sinon.assert.calledOnce(this.pl1GetResSpy);
+        sinon.assert.calledOnce(this.pl2GetResSpy);
+        
+        // player3 ist bot, sollte daher nicht aufgerufen werden
+        sinon.assert.notCalled(this.pl3GetResSpy);
+        
+        sinon.assert.calledOnce(this.res1SendSpy);
+        sinon.assert.calledOnce(this.res2SendSpy);
+    }
+    
 });
