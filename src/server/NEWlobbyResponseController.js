51a2f48530ee362589c3bada615f16b87f5fa883
y7haar <y7haar@gmail.com>
2015-06-28 23:32:01 +0200

    Implemented functions for accepting EventSource

--- src/server/NEWlobbyResponseController.js
+++ src/server/NEWlobbyResponseController.js
@@ -7,6 +7,24 @@ tddjs.namespace("server.controller").NEWlobbyResponseController= lobbyResponseCo
 function lobbyResponseController()
 {   
    var _lobby;
+   var _lobbyController = tddjs.server.controller.lobbyController.getInstance();
+   
+   
+   function setLobbyById(aId)
+   {
+       _lobby = _lobbyController.getLobbyById(aId);
+   }
+   
+   function acceptEventSource(req, res)
+   {
+       if(req.get("accept") === "text/event-stream")
+       {
+           res.append("content-type", "text/event-stream");
+           res.append("cache-control", "no-cache");
+           res.append("connection", "keep-alive");
+       }
+   }
+   
    
    
     //test
@@ -15,4 +33,8 @@ function lobbyResponseController()
             return _lobby;
         }
     });
+    
+    
+    this.setLobbyById = setLobbyById;
+    this.acceptEventSource = acceptEventSource;
 }
