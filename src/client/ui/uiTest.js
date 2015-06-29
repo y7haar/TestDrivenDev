fb8697dd9e491f0bd9dd89d2b6451659ef8f6774
René Lottes <rene.lottes@hof-university.de>
2015-06-29 10:57:11 +0200

    have to commit don't know why

--- src/client/ui/uiTest.js
+++ src/client/ui/uiTest.js
@@ -27,7 +27,6 @@ function startGame(){
             m2 = m.serializeAsJSON(m2);
             //controller._getMap(m2);
             //controller._initMap();
-            controller.addButton( new tddjs.client.ui.button(30,canvas.height-60,"TEST",ctx));
             //window.requestAnimationFrame(controller.drawLoading);
             controller.init(m2);
        }
