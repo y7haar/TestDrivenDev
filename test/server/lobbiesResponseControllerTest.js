32da509f84ddd447cfcba17dfe42850f9ebae94c
y7haar <y7haar@gmail.com>
2015-06-30 23:39:36 +0200

    Implemented test if respondAllLobbies returns all lobbies

--- test/server/lobbiesResponseControllerTest.js
+++ test/server/lobbiesResponseControllerTest.js
@@ -52,14 +52,22 @@ TestCase("LobbyResponseControllerTest", {
         // Must be changed 
         this.lrc = new tddjs.server.controller.lobbiesResponseController();
         this.lobbyController = tddjs.server.controller.lobbyController.getInstance();
-
+        
         // Needed because of Singleton
         this.lobbyController.getLobbies().length = 0;
+        
+        this.lobbyFactory = new tddjs.server.controller.lobbyFactory();
+        this.lobby = this.lobbyFactory.getNewLobby();
+        this.lobbyController.addLobby(this.lobby);
+        
         
         this.sandbox = sinon.sandbox.create();
         
         this.req = new fakeReq();
         this.res = new fakeRes();
+        
+        this.lobbyControllerSerializeOSpy = this.sandbox.stub(this.lobbyController, "serializeAsObject");
+        this.resJsonSpy = this.sandbox.stub(this.res, "json");
     },
     tearDown: function ()
     {
@@ -67,8 +75,30 @@ TestCase("LobbyResponseControllerTest", {
         this.lobbyController.getLobbies().length = 0;
         delete this.lrc;
     },
+    
     "test constructor should create new instance of controller": function () {
         assertObject(this.lrc);
+    },
+    
+    "test lrc should have function to get all lobbies that are not started": function () {
+        assertFunction(this.lrc.respondAllLobbies);
+    },
+    
+    "test respondAllLobbies should call serialize method in lobbyController": function () {
+        sinon.assert.notCalled(this.lobbyControllerSerializeOSpy);
+        
+        this.lrc.respondAllLobbies();
+        
+        sinon.assert.calledOnce(this.lobbyControllerSerializeOSpy);
+    },
+    
+    "test respondAllLobbies should call res.json with serialized lobbies": function () {
+        sinon.assert.notCalled(this.resJsonSpy);
+        
+        this.lrc.respondAllLobbies();
+        
+        sinon.assert.calledOnce(this.resJsonSpy);
+        sinon.assert.calledWith(this.resJsonSpy, this.lobbyController.serializeAsObject());
     }
     
 });
