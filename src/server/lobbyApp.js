12480b73860eda2ee66ed5fd50c4c0437b206559
y7haar <y7haar@gmail.com>
2015-07-01 01:05:48 +0200

    Updated lobby App with new Controller

--- src/server/lobbyApp.js
+++ src/server/lobbyApp.js
@@ -17,6 +17,11 @@ require("./lobbyController");
 require("./lobbyFactory");
 require("./lobbyResponseController");
 
+var LobbyResponseController = require("./NEWlobbyResponseController");
+var LobbiesResponseController = require("./lobbiesResponseController");
+var lobbyResponseController = new LobbyResponseController();
+var lobbiesResponseController = new LobbiesResponseController();
+
 var lobbyApp = express();
 
 lobbyApp.use(bodyParser.json({}));
@@ -24,16 +29,17 @@ lobbyApp.use(bodyParser.json({}));
 lobbyApp.use(sessions({
   cookieName: 'session',
   secret: 'usdnzfu303un04fu43fnpp09suwendwe', 
-  duration: 1000 * 20, // 1 Day valid 24 * 60 * 60 * 1000
-  activeDuration: 1000 * 30 //1000 * 60 * 5 
+  duration: 1000 * 60 * 60, // 1 Day valid 24 * 60 * 60 * 1000
+  activeDuration: 1000 * 60 * 5 //1000 * 60 * 5 
 }));
 
 
 var lobbyController = tddjs.server.controller.lobbyController.getInstance();
 
 lobbyApp.get("", function (req, res) {
-    var lobbyResponseController = new tddjs.server.controller.lobbyResponseController();
-    res.send(lobbyController.serialize());
+    
+    lobbiesResponseController.respondAllLobbies(req, res);
+
 });
 
 lobbyApp.post("", function (req, res) {
@@ -54,45 +60,31 @@ lobbyApp.post("", function (req, res) {
 });
 
 lobbyApp.post("/:id", function (req, res) {
+    
     try
     {
-        var lobbyResponseController = new tddjs.server.controller.lobbyResponseController();
-        var response = lobbyResponseController.switchLobbyPostTypes(parseInt(req.params.id), req.body);
-        
-        if(typeof req.session.token ==="undefined")
-            req.session.token = lobbyResponseController.getToken();
-        
-        if(typeof response !== "undefined")
-            res.json(response);
-        
-        else
-            res.sendStatus(200);
+        var lrc = new LobbyResponseController();
+        lrc.setLobbyById(parseInt(req.params.id));
+        lrc.respondByType(req, res);
     }
-    
     catch(e)
     {
-        console.dir(e);
-        res.status(400).send("Wrong JSON Format");
+        res.sendStatus(400);
     }
 });
 
 lobbyApp.get("/:id", function (req, res) {
-    if(req.accepts("text/event-stream"))
+    try
     {
-        res.type("text/event-stream");
-        res.connection.setTimeout(0);
-        
-        var lobby = lobbyController.getLobbyById(req.params.id);
-        var player = lobby.getPlayerByToken(req.session.token.toString());
-        player.setResponseObject(res);
-        
-        console.log("EventSource am Start");
-        console.log(req.session.token);
-        console.log(lobby.getPlayers()[0].getToken());
+        var lrc = new LobbyResponseController();
+        lrc.setLobbyById(parseInt(req.params.id));
+        lrc.acceptEventSource(req, res);
     }
-    //res.end();
+    
+    catch(e)
+    {
+        res.sendStatus(400);
+    }
 });
-
-
 
 module.exports = lobbyApp;
