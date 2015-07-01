a1019338f470ca56d026e02eef6efa2890628696
René Lottes <rene.lottes@hof-university.de>
2015-07-01 17:01:56 +0200

    added sourcecode for mapController

--- src/server/mapController.js
+++ src/server/mapController.js
@@ -16,15 +16,26 @@ function mapController()
     }
     
     function getMap(){
+        if(!_map)
+            throw new Error("Call init first!");
         return _map;
     }
     
     function getSerializedMap(){
-        
+        if(!_jsonMap)
+            throw new Error("Call init first!");
+        return _jsonMap;
     }
     
+    function reset(){
+        _map = null;
+        _player=[];
+        _jsonMap = null;
+    }
     
     this.init = init;
     this.getMap = getMap;
     this.getSerializedMap = getSerializedMap;
+    
+    this.reset = reset;
 }
