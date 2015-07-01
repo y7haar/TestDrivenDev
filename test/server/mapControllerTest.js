aafe9c8566b77899e59a695597ceb1718d355337
René Lottes <rene.lottes@hof-university.de>
2015-07-01 16:39:45 +0200

    added basic TestCase-Structure for mapController

--- test/server/mapControllerTest.js
+++ test/server/mapControllerTest.js
@@ -3,3 +3,20 @@
  */
 
 
+TestCase("MapControllerTest", {
+    setUp: function (){
+   
+    }, 
+    tearDown: function (){
+
+    },
+    
+    "test object of mapController should not be undefined": function(){  
+        
+    },
+    
+    "test if mapController has a instance of mapGenerator after init": function(){  
+        
+    }
+    
+});
