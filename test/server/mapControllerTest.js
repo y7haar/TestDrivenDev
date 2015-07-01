9ac73a951d81572f1c0b7612e78d420f8a13d5da
René Lottes <rene.lottes@hof-university.de>
2015-07-01 16:50:21 +0200

    added some testcases for mapController

--- test/server/mapControllerTest.js
+++ test/server/mapControllerTest.js
@@ -8,31 +8,45 @@ TestCase("MapControllerTest", {
         this.mapCon = tddjs.server.controller.mapController();
     }, 
     tearDown: function (){
-
+        delete this.mapCon;
     },
     
     "test object of mapController should not be undefined": function(){  
-        
+        assertObject(this.mapCon);
     },
     
     "test if mapController has a instance of mapGenerator after init": function(){  
+        this.mapCon.init();
+        var mapGen = this.mapCon.getMapGenerator();
         
+        assertInstanceOf(tddjs.server.controller.mapController,mapGen);
     },
     
     "test if mapController return valid serverMap": function(){  
+        this.mapCon.init();
+        //TODO: strukture überlegen, wie was aufgerufen werden muss
+        this.mapCon.setPlayer();
         
+        var map = this.mapCon.getMap();
+        //TODO: test map
     },
     
     "test if mapController return valid serialized clientMap": function(){  
-        
+        //TODO: test für serialisierte map
     },
     
     "test mapController should return same map if not reseted": function(){  
+        //TODO: gleicher test wie "test if mapController return valid serverMap"
+        // nur 2mal, um zu testen ob 2mal die selbe map kommt
         
+        var map = this.mapCon.getMap();
+        var map2 = this.mapCon.getMap();
+        
+        assertEquals(map,map2);
     },
     
     "test if mapController produce different mapSizes for different Playercount": function(){  
-        
+        //TODO: test dass unterschiedliche gridSizes gibt
     }
     
 });
