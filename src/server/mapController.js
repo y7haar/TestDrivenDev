4b386edef38f084b9eac32f3286e6806fe7fc6cd
René Lottes <rene.lottes@hof-university.de>
2015-07-01 17:09:35 +0200

    added sourcecode for mapController

--- src/server/mapController.js
+++ src/server/mapController.js
@@ -11,9 +11,30 @@ function mapController()
     var _player=[];
     var _jsonMap;
     
+    var _mapGenerator = new tddjs.server.controller.mapGenerator();
+    
     function init(){
+        //TODO: switch-case for playercount
+        setUp2Player();
         
     }
+    
+    // <editor-fold defaultstate="collapsed" desc="init-functions">
+    
+    function setUp2Player(){
+        _mapGenerator.setGridSize(150,150);
+        _mapGenerator.setMaximumCountrySize(300);
+        _mapGenerator.setMinimumCountrySize(200);
+        _mapGenerator.setMinimumContinentNumber(6);
+        _mapGenerator.setMaximumContinentNumber(10);
+        _mapGenerator.setMinimumContinentSize(4);
+        _mapGenerator.setMinimumWaterNumber(6);
+        _mapGenerator.setMinimumWaterNumber(4);
+        
+        _map = _mapGenerator.generateMap();
+    }
+    
+    // </editor-fold>
     
     function getMap(){
         if(!_map)
@@ -22,8 +43,9 @@ function mapController()
     }
     
     function getSerializedMap(){
-        if(!_jsonMap)
+        if(!_map)
             throw new Error("Call init first!");
+        //TODO: serialisieren
         return _jsonMap;
     }
     
@@ -33,9 +55,15 @@ function mapController()
         _jsonMap = null;
     }
     
+    function getMapGenerator(){
+        return _mapGenerator;
+    }
+    
     this.init = init;
     this.getMap = getMap;
     this.getSerializedMap = getSerializedMap;
     
     this.reset = reset;
+    
+    this.getMapGenerator = getMapGenerator;
 }
