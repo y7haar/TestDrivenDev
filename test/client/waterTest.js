a12c035205d21854b6301ad27b11ec615b9954b8
Ralf Heinrich <ralf.heinrich@hof-university.de>
2015-07-02 17:48:31 +0200

    Testfälle besser getrennt

--- test/client/waterTest.js
+++ test/client/waterTest.js
@@ -32,21 +32,30 @@ TestCase("WaterBorderTest", {
         assertEquals(0, this.water.getBorders().length);
     },
     
-    "test water should store other countrys, exception if not a Country": function () {
-        
-        var fakeCountry = {name:"Franzaken"};
-        var water = this.water;
-        
-        assertException(function(){
-            water.addBorder(fakeCountry);
-        },"TypeError");
-        
+    "test water should store other countrys, exception if not a Country": function ()
+    {   
         assertEquals(0, this.water.getBorderCount());
         this.country1.addBorder(this.country1,this.country2);
         assertEquals(1, this.water.getBorderCount());
         this.country1.addBorder(this.country1,this.country3);
         assertEquals(2, this.water.getBorderCount());
-
+    },
+    
+    "test Shouldnt store something thats not a country": function()
+    {
+        var country = this.country1;
+        var fakeCountry = {name:"Franzaken"};
+        var water = this.water;
+        
+        assertException(function()
+        {
+            water.addBorder(fakeCountry, country);
+        },"TypeError");
+        
+        assertException(function()
+        {
+            water.addBorder(country, fakeCountry);
+        },"TypeError");
     }
 });
 
@@ -58,31 +67,45 @@ TestCase("WaterNameTest", {
     {
         this.water = null;
     },
+    
     "test country shoulde store name as a String": function () {
 
         assertFunction(this.water.setName);
         assertFunction(this.water.getName);
 
         var name = "Usee";
-        var fakeName = {name: "Schwarzes Meer"};
         var water = this.water;
-        
-        assertException(function(){
-            water.getName();
-        },"initError");
         
         assertNoException(function () {
             water.setName(name);
         });
+        assertEquals(name, this.water.getName());
         
+        name = "Rotes Meer";
+        this.water.setName(name);
+        assertEquals(name, this.water.getName());
+    },
+    
+    "test Shouldnt be able to set incorrect Names": function()
+    {
+        var name = "Usee";
+        var water = this.water;
+        var fakeName = {name: "Schwarzes Meer"};
+        
+        this.water.setName(name);
         assertException(function () {
             water.setName(fakeName);
         }, "TypeError");
 
         assertEquals(name, this.water.getName());
-        name = "Rotes Meer";
-        this.water.setName(name);
-        assertEquals(name, this.water.getName());
-
+    },
+    
+    "test Shouldnt be able to get Name before it was setted": function()
+    {
+        var water = this.water;
+        
+        assertException(function(){
+            water.getName();
+        },"initError");
     }
 });
