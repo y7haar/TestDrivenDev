2b195a2fb0a021079f05130d6d47882ce15f1138
y7haar <y7haar@gmail.com>
2015-07-05 19:56:24 +0200

    Implemented algorithm to perform a single attack

--- src/server/randomAi.js
+++ src/server/randomAi.js
@@ -15,10 +15,12 @@ function randomAi(aPlayer, aMap, aGlc)
     var _gameLoopController = aGlc;
     
     var _self = this;
+    var _instantAttackValue = 2.5;
+    var _attackWorthy = 1.5;
     
     function evaluateAttack(aFrom, aTo)
     {
-        return aFrom.getUnitCount() / aTo.getUnitCount();
+        return (aFrom.getUnitCount() - 1) / aTo.getUnitCount();
     }
     
     function evaluatePlacing(aCountry)
@@ -97,10 +99,72 @@ function randomAi(aPlayer, aMap, aGlc)
         _self.placeUnits(maxValueCountry, unitsLeft);
     }
     
+    function attack()
+    {
+        var continents = _map.getContinentsByPlayer(_player);
+        var maxValue = 0;
+        var bestFrom, bestTo;
+        
+        for (var continent in continents) 
+        {
+            if (continents.hasOwnProperty(continent)) 
+            {
+                var countries = continents[continent].getCountriesByPlayer(_player);
+                
+                for(var country in countries)
+                {
+                    if(countries.hasOwnProperty(country))
+                    {
+                        // Evaluate all enemy borders
+                        var borders = countries[country].getBorders();
+                        
+                        for(var i = 0;i < borders.length;++i)
+                        {
+                            // Enemy border
+                            if(borders[i].getOwner() !== _player)
+                            {
+                                var value = _self.evaluateAttack(countries[country], borders[i]);
+                                
+                                if(value >= _instantAttackValue)
+                                {
+                                    _gameLoopController.attack(countries[country], borders[i]);
+                                    return true;
+                                }
+                                
+                                if(value > maxValue)
+                                {
+                                    maxValue = value;
+                                    bestFrom = countries[country];
+                                    bestTo = borders[i];
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+        // check if attack is worth
+        if(maxValue >= _attackWorthy)
+        {
+            _gameLoopController.attack(bestFrom, bestTo);
+            return true;
+        }
+        
+        return false;
+    }
+    
+    function attackAll()
+    {
+        
+    }
+    
     this.evaluateAttack = evaluateAttack;
     this.evaluatePlacing = evaluatePlacing;
     this.placeUnits = placeUnits;
     this.placeAllUnits = placeAllUnits;
+    this.attack = attack;
+    this.attackAll = attackAll;
 };
 
 
