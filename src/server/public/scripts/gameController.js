4785fb457401b12176ef7f8de0ef879e1d8efff8
René Lottes <rene.lottes@hof-university.de>
2015-07-09 04:48:21 +0200

    this should fix the attacking error

--- src/server/public/scripts/gameController.js
+++ src/server/public/scripts/gameController.js
@@ -21,6 +21,7 @@ function gameController(aCtx){
     var ajax = tddjs.util.ajax;
     
     var _selected=[];
+    var _attack={};
     
     function init(){
         _gameUiController = new tddjs.client.ui.gameUiController(aCtx);
@@ -100,6 +101,12 @@ function gameController(aCtx){
         if(arguments.length === 1){
             _unitsLeft = req.unitCount;
         }
+        for(var c in _selected){
+            for(var i in _selected[c].getBorders()){
+                _selected[c].getBorders()[i].activ=false;
+            }
+        }
+        _selected=[];
         _gameUiController.updateUnitCounts();
         _gameUiController.drawGame();
     }
@@ -179,8 +186,39 @@ function gameController(aCtx){
         var id = _gridMap[x][y].id;
         
         if(id>=0){ //kein wasser
-            _gridMap[x][y].selected=!_gridMap[x][y].selected;
+            if(!_gridMap[x][y].selected & _selected.length < 2){
+                if(_gridMap[x][y].getOwner() === _Player){
+                    _attack.from = _gridMap[x][y];
+                    _gridMap[x][y].selected=true;
+                    _selected.push(_gridMap[x][y]);
+                }
+                else{
+                    _attack.to = _gridMap[x][y];
+                    _gridMap[x][y].selected=true;
+                    _selected.push(_gridMap[x][y]);
+                }
+            }
             
+            if(_gridMap[x][y].selected){
+                if(_gridMap[x][y].getOwner() === _Player)
+                    _attack.from = null;
+                else
+                    _attack.to = null;
+                _gridMap[x][y].selected=false;
+                _selected.splice(_selected.indexOf(_gridMap[x][y]),1);
+            }
+            
+            
+            /*
+            if(!_gridMap[x][y].selected & _selected.length < 2){
+                _gridMap[x][y].selected=true;
+                _selected.push(_gridMap[x][y]);
+            }
+            else if(_gridMap[x][y].selected){
+                _gridMap[x][y].selected=false;
+                _selected.splice(_selected.indexOf(_gridMap[x][y]),1);
+            }
+            /*
             if(_gridMap[x][y].selected){
                 if(_selected.length < 2)
                     _selected.push(_gridMap[x][y]);
@@ -193,11 +231,13 @@ function gameController(aCtx){
                 for(var i in _gridMap[x][y].getBorders()){
                     _gridMap[x][y].getBorders()[i].activ=false;
                 }
-            }
+            }*/
             for(var c in _selected){
                 for(var i in _selected[c].getBorders()){
-                    if(_selected.length < 2)
-                        _selected[c].getBorders()[i].activ=true;
+                    if(_selected.length < 2){
+                        if(!_selected[c].getBorders()[i].getOwner() === _Player)
+                            _selected[c].getBorders()[i].activ=true;
+                    }
                     else
                         _selected[c].getBorders()[i].activ=false;
                 }
@@ -218,14 +258,13 @@ function gameController(aCtx){
             window.alert("Please place all units!");
     }
     function attackingButtonAttack(){
-        var from;
-        var to;
-        for(var i in _selected){
-            if(_selected[i].getOwner() === _Player)
-                from = _selected[i];
-            else
-                to = _selected[i];
+        if(_attack.from === null || _attack.to === null){
+            console.log(_attack);
+            return;
         }
+        
+        var from = _attack.from;
+        var to = _attack.to;
         var attackMove = {
             type: 'attack',
             from: {
